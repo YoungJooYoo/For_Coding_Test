0e3890b181e5a5877d9caa291cdb225ef7d135a5
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2021-11-29 07:35:36 +0900

    Time: 4 ms (71.12%), Space: 6.6 MB (67.86%) - LeetHub

---
+++ find-the-difference/find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        for(size_t i = 0; i < t.length(); i++) {
+            if(s[i] != t[i]) {
+                return t[i];
+            }
+        }
+        
+        return 'success';
+    }
+};
