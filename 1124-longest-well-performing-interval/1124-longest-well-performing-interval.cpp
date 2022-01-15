ad33b2bcf0dbdfd350023ac1beb80aa7db358818
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-01-16 02:05:19 +0900

    Time: 28 ms (81.01%), Space: 23.1 MB (70.03%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
-     int longestWPI(vector<int>& hours) {
-        int res = 0, score = 0, n = hours.size();
+     int longestWPI(vector<int>& hours) 
+     {
+        const size_t n = hours.size();
         unordered_map<int, int> seen;
+        int res = 0; 
+        int score = 0; 
+        
         for (int i = 0; i < n; ++i) {
             score += hours[i] > 8 ? 1 : -1;
             if (score > 0) {
@@ -10,8 +14,10 @@ public:
             } else {
                 if (seen.find(score) == seen.end())
                     seen[score] = i;
-                if (seen.find(score - 1) != seen.end())
+                if (seen.find(score - 1) != seen.end()) {
                     res = max(res, i - seen[score - 1]);
+                }
+                    
             }
         }
         return res;
