7ba1b75f6215b3dec96a31ba41636ee2a43a7f6f
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-01-17 00:29:45 +0900

    Time: 7 ms, Memory: 8.4 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -7,9 +7,9 @@ public:
         int max_value = 0;
         
         for (int i = 0; i < s.length(); i++) {
-            start = max(start, dictionary[s[i]]);  //
-            dictionary[s[i]] = i; // 이 문자가 인덱스 어디에서 나왔는지 저장
-            max_value = max(max_value, i - start); // i - start = length
+            start = max(start, dictionary[s[i]]);  // 문자 사전 ex) 'a' = 97, 97번 인덱스가 start
+            dictionary[s[i]] = i; // 이 문자가 인덱스 어디에서 나왔는지 저장, 97번('a')는 i번째에서 등장
+            max_value = max(max_value, i - start); // i - start = length, 즉, 해당 문자 중복으로 나올때 체크
         }
         
         return max_value;
