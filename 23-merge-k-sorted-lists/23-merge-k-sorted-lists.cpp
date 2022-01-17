642f1e717522710e9737b1d38527c2ef88b807f9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-01-18 00:38:25 +0900

    Time: 48 ms (34.01%), Space: 14.1 MB (15.62%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) 
+    {
+		vector<int> nums;
+        
+        if (lists.empty() == true) {
+            return NULL;
+        }
+
+		for (size_t i = 0; i < lists.size(); ++i) {
+			ListNode* node = lists[i];
+			while (node != NULL) {
+				nums.push_back(node->val);
+				node = node->next;
+			}
+		}
+
+		sort(nums.begin(), nums.end());
+		
+		ListNode* result = new ListNode(0); // init val = 0;
+		ListNode* head = result;
+		for (size_t i = 0; i < nums.size(); ++i) {  
+			result->next = new ListNode(nums[i]); 
+			result = result->next;
+		}
+        // 값이 0인 head만들고 head->next부터, 1개씩 노드 만들어가면서 val을 넣어줌
+        // head에 val = 0 이 있으므로, head->next부터 merge된 노드이다.
+        
+		return head->next;
+    }
+};
