b125cf85b5688da5c0446d5587d591e14c6d2ae6
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-01-30 15:44:22 +0900

    Time: 7 ms (34.54%), Space: 11.1 MB (28.69%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
+        int start = 0;
+        int mid;
+        int end = nums.size() - 1;
         
-        while(low <= high){
-            // int mid = (low + high) / 2;
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] >= nums[low]){
-                //left half is sorted
-                if(target >= nums[low] && target < nums[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1;
+        while(start <= end) {
+            mid = start + (end - start) / 2; // or //mid = (start + end) / 2;
+            if(nums[mid] == target) {
+                return mid;
             }
-            else{
+            if(nums[mid] >= nums[start]) {
+                //left half is sorted
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            else {
                 //right half is sorted
-                if(target  > nums[mid] && target <= nums[high])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
+                if (target  > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
             }
         }
         
@@ -29,12 +35,12 @@ public:
 };
 
 /*
-풀이법 : 
+풀이법 :
 mid 인덱스 기준으로 좌측 오른차순 정렬 우측 오른차순 정렬로 나뉘었다.
 
-target을 중간 인덱스 값과, 마지막 인덱스 과 비교해서, 
+target을 중간 인덱스 값과, 마지막 인덱스 과 비교해서,
 어디서 탐색 시작할지 경정
-target이 오른쪽 작은 숫자 배열의 최대 값보다 크면 왼쪽 배열탐색 
+target이 오른쪽 작은 숫자 배열의 최대 값보다 크면 왼쪽 배열탐색
 이런식으로 나아간다.
 
 */
