76aa18e951592972d6b0fa5c9175ffa9f320c7ea
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-01-31 01:30:19 +0900

    Time: 227 ms (32.26%), Space: 28.3 MB (15.62%) - LeetHub

--- 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.cpp
+++ 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.cpp
@@ -7,7 +7,6 @@ public:
         nums.insert(number);
     }
     
-    /** Find if there exists any pair of numbers which sum is equal to the value. */
     bool find(int value)
     {
         for (auto itr = nums.begin(); itr != nums.end(); itr++) {
@@ -23,3 +22,15 @@ public:
     private:
     multiset<long> nums; // 중복이 허용되는 자료구조 set
 };
+
+/* 
+    아이디어 :  1 + 4 = 5    ->    4 = 5 - 1로 접근하기  
+    4를 찾는중 5에서 무엇을떄뱌 4가 나오는지 찾는다.
+    
+    찾는 target이 4라고 가정을 하자.
+    자료구조 nums에 저장된 자료에 순차 접근하면서
+    
+    {1 3 4 5 7} 저장 되어있다는 가정하에
+    target(4) = 1 - *itr 접근
+    
+*/
