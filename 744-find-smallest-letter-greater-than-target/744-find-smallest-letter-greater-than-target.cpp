0fd314d3fd8140c464512b55038e1c696d40a1a1
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-01-31 19:04:46 +0900

    Time: 26 ms (30.20%), Space: 15.9 MB (69.13%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -8,7 +8,7 @@ public:
         
         while (start <= end) {
             mid = start + (end -start) / 2; // or mid = (start + end) / 2;
-            if (letters[mid] <= target) {
+            if (letters[mid] <= target) { // 문제가 원하는 리턴 값은 타겟보다 최소로 커야한다.
                 start = mid + 1;
             }
             else { // letters[mid] > target
@@ -16,10 +16,11 @@ public:
             }
         }
         
-        if (letters[start] > target){
+        // 
+        if (letters[start] > target) {  // 이진탐색으로 찾은 start인덱스의 letters값이 target보다 크다면 반환
 			return letters[start];
 		}
         
-		return letters[0];
+		return letters[0]; // 문제 조건에 따라, 없다면 배열의 맨 앞을 반환
     }
 };
