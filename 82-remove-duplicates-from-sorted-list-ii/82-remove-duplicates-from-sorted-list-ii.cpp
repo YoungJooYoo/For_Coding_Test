fbc6ba2dd364a07dc30cda77ef821b0eabc34cb0
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-01 00:14:10 +0900

    Time: 4 ms (95.12%), Space: 11.1 MB (58.18%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -1,68 +1,21 @@
-/*
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head)
-    {
-        if(head != nullptr) {
-            return head;
-        }
-        
-        ListNode* prev_node = nullptr;
-        ListNode* curr_node = head; 
-        ListNode* next_node = curr_node->next;
-        
-        while (next_node != nullptr) {
-            if (curr_node->val == next_node->val) {
-                while(next_node && next_node->val == curr_node->val)
-                    next_node = next_node->next;
-                if(prev_node == nullptr) // This condition means element at the head is repeating. So, head pointer needs to be shifted.
-                    head = next_node;
-                else
-                    prev_node->next = next_node;
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* cur = dummy;
+        int duplicate;
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                duplicate = cur->next->val;
+                while (cur->next && cur->next->val == duplicate) {
+                    cur->next = cur->next->next;
+                }
             }
             else {
-                prev_node = curr_node;
-            }
-            curr_node = next_node;
-            if (next_node != nullptr) {
-                next_node = curr_node->next;
+                cur = cur->next;
             }
         }
-        
-        return head;
-    }
-}; 
-*/
-
-class Solution
-{
-public:
-    ListNode* deleteDuplicates(ListNode* head)
-    {
-        if(!head)
-            return head;
-        
-        ListNode *prev=NULL, *curr=head, *next=curr->next;
-        while(next)
-        {
-            if(curr->val == next->val)
-            {
-                while(next && next->val == curr->val)
-                    next=next->next;
-                if(!prev) // This condition means element at the head is repeating. So, head pointer needs to be shifted.
-                    head=next;
-                else
-                    prev->next = next;
-            }
-            else
-            {
-                prev=curr;
-            }
-            
-            curr=next;
-            if(next)
-                next=curr->next;
-        }
-        return head;
+        return dummy->next;
     }
 };
