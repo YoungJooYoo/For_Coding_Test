007600e62e52e8a0d384e2e0733a549537671ccf
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-02 17:07:20 +0900

    Time: 85 ms (5.13%), Space: 32.7 MB (30.70%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -6,15 +6,22 @@ public:
             return NULL;
         }
         
-        if (root->val == key) { // key를 가진 노드를 찾을때
-            if(root->left == NULL)  return root->right;
-            if(root->right == NULL) return root->left;
-            if(root->right!=NULL && root->left!=NULL){
-                TreeNode *temp = root->right;
-                while(temp->left!=NULL) temp = temp->left;
-                root->val = temp->val;
+        if (root->val == key) { // key를 가진 노드를 찾은 경우
+            if (root->left == NULL)  {
+                return root->right;
+            }
+            if (root->right == NULL) {
+                return root->left;
+            }
+            /* 이 구간은, 일단 key value를 가진 노드를 찾으면, 
+                그 노드의 오른쪽 val 값이  답이니 일단 temp에 그 값을 넣는다. */
+            if (root->right != NULL && root->left != NULL) {
+                TreeNode* temp = root->right; // 찾아야하는 값이 key보다 큰 최소값이다. 결국 오른쪽 노드의 value이다.
+                while (temp->left != NULL) { // key를 가진 node의 왼뽁주터 탐색, 왼쪽에 가장 작은 value가 있으므로
+                    temp = temp->left; // 당장 right노드가 없다면 왼쪽노드 끝까지 탐색, 마지막 노드의 오른쪽 값이 value 
+                }
+                root->val = temp->val; // key를 가진 node에, 찾아낸 key node->val보다 크지만 최소값을 넣는다.
                 root->right = deleteNode(root->right,root->val);
-                
             }
         }
         else { // key를 찾지 못한경우 계속 탐색
