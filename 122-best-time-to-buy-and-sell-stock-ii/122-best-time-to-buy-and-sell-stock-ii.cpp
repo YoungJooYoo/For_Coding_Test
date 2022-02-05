5cc3264bc24963e518097026fe04dc97c046610b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-05 16:18:20 +0900

    Time: 10 ms (37.20%), Space: 13 MB (46.38%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int profit = 0;
+        int valley;
+        int peak;
+        
+        for (size_t i = 0; i < prices.size(); i++) {
+            while (i < prices.size() - 1 && prices[i] >= prices[i + 1]) {
+                i++;
+            }
+            valley = prices[i];
+            while (i<prices.size() - 1 && prices[i] <= prices[i + 1]) {
+                i++;
+            }
+            peak = prices[i];
+            profit += peak - valley;
+        }
+        
+        return profit;
+    }
+};
+
