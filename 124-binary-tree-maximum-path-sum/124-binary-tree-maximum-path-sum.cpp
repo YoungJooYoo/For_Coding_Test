da13c512015ebdbb18b3c561a4ca07cd74f32585
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-05 20:44:45 +0900

    Time: 34 ms (47.90%), Space: 27.7 MB (69.03%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -6,7 +6,6 @@ public:
             return 0;
         }
         
-        //int aaa[40000];
         int left_sum = max(dfs(root->left), 0);
         int right_sum = max(dfs(root->right), 0);
             
