00b18d2e4c2ca5d44167d4d5b693cca123812d71
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-12 15:28:09 +0900

    Time: 14 ms (43.33%), Space: 7.5 MB (90.44%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    ListNode* detectCycle(ListNode* head) 
-    {
-        ListNode* curr = head;
-        unordered_map<ListNode*, int> mp;
-            
-        while (curr != nullptr) {
-            mp[curr]++;
-            if (mp[curr] > 1) {
-                return curr;
-            }
-            curr = curr->next;
+    // Space complexity: O(1).
+    // Floyd's cycle-detection algorithm (tortoise and hare)
+    ListNode *detectCycle(ListNode *head) {
+        if (head == nullptr) {
+            return nullptr;
         }
-        
-        return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if (fast == slow) {
+                // Find meeting point where cycle begins
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                
+                return slow;
+            }
+        }
+
+        return nullptr;       
     }
 };
-
-/*
-풀이법 :
-해시 자려구조 맵을 사용해
-중복 접근이 일어나는 최초의 구간이 사이클 시작하는 인덱스인 것을 알 수 있다.
-따라서 그 구간을 리턴하면 답이 된다.
-*/
