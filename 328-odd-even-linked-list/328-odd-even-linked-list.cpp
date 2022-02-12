6f5ca11f27b4bc6cf64cd7fe7ab85bc427d5dd93
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-12 15:49:57 +0900

    Time: 17 ms (44.03%), Space: 10.7 MB (5.77%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) 
@@ -21,6 +11,7 @@ public:
         ListNode* curr = head;
         size_t i = 0;
         
+        // 짝수인덱스 홀수인덱스 따로 구분하면서 배열에 값을 저장한다.
         while (curr != nullptr) {
             if (i % 2 == 0) { // 짝수인덱스
                 even_index.push_back(curr->val);
@@ -32,6 +23,7 @@ public:
             i++;
         }
         
+        // 문제에서 요구하듯 짝수인덱스 먼저 리스트에 넣고, 그 다음 홀수 인덱스 값을 이어서 넣는다.
         curr = head;
         
         for (size_t i = 0; i < even_index.size(); i++) {
@@ -46,3 +38,9 @@ public:
         return head;
     }
 };
+
+/*
+풀이법 :
+짝수, 홀수 인덱스를 구분해서 배열에 담는다.
+배열에 담긴 값을 리스트를 순회하면서 짝수 먼저 넣고, 홀수먼저 넣는다.
+*/
