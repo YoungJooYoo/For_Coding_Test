02b1e0fb060304de6177be5ff42f8a2e5e44949d
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-14 00:06:44 +0900

    Time: 53 ms (5.09%), Space: 19.8 MB (9.13%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -7,7 +7,7 @@ public:
         int count = 0;
         
         /* 자료 중복이 없는 해시맵에 숫자-카운트 묶어서 계산 */
-        for (size_t i= 0 ; i < n; i++) {
+        for (size_t i = 0 ; i < n; i++) {
             mp[nums[i]]++;
         }
         
