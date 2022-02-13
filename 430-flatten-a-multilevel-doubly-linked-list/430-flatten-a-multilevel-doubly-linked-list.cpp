af9aa0df743c342f9f56da9226ffdf151c6d4fb4
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-14 07:44:51 +0900

    Time: 9 ms (21.25%), Space: 7.3 MB (93.01%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     Node* flatten(Node* head) 
     {
+        Node* next;
+        Node* p
+            ;
         for (Node* curr = head; curr != nullptr; curr = curr->next) {
-            if (curr->child) {
-                Node* next = curr->next;
+            if (curr->child) { // child node에 다음 노드가 있는지 먼저 찾는다.
+                next = curr->next; // 
                 curr->next = curr->child;
                 curr->next->prev = curr;
                 curr->child = NULL;
-                Node* p = curr->next;
+                p = curr->next;
                 
                 while (p->next) {
                     p = p->next;
