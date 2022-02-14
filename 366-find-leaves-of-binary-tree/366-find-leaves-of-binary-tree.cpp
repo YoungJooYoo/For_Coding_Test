99b35c16f71179a9b8dfb54001ffbf0f3a5846a4
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-15 07:44:20 +0900

    Time: 4 ms (46.07%), Space: 9.8 MB (5.94%) - LeetHub

---
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isLeaf(TreeNode* root){
+        return root->left == nullptr && root->right == nullptr;
+    }
+    TreeNode* dfs(TreeNode* root, vector<int> &v, vector<vector<int>> ans){
+        if(root == nullptr)
+            return root;
+        if(isLeaf(root)){
+            v.push_back(root->val);
+            return nullptr;
+        }
+        root->left = dfs(root->left,v,ans);
+        root->right = dfs(root->right,v,ans);
+        return root;
+    }
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        vector<vector<int> > ans;
+        while(root){
+            vector<int> v;
+            root = dfs(root,v,ans);
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
