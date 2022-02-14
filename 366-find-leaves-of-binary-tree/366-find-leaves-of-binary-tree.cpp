c9b04146acd3d15cae0d3a487a105d0999bd6aa4
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-15 07:58:36 +0900

    Time: 4 ms (46.07%), Space: 8.2 MB (89.05%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -15,6 +15,8 @@ public:
             return nullptr;
         }
         
+        // dfs 시행할따마다 leaf는 nullptr이 되어 삭제가 된다.
+        // 즉 while 한번씩 돌떄마다, 문제에서 제시한 모양의 트리로 바뀌어간다.
         root->left = dfs(root->left);
         root->right = dfs(root->right);
         
@@ -37,3 +39,10 @@ private:
     vector<int> temp;
     
 };
+
+/*
+풀이법 :
+방문한 트리를 제거하고,
+dfs를 다시 반복해서 시행함으로
+방문을 도출한다.
+*/
