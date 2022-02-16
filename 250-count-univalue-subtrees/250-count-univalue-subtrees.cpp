41c99c62e25291f8c03e4b85c8622a9848263397
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-17 07:59:00 +0900

    Time: 14 ms (17.59%), Space: 16.4 MB (61.35%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.cpp
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-bool countUnivalSubtreesRecursive(TreeNode* root, int& count)
+bool count_unival_subtrees_recursive(TreeNode* root, int& count)
     {
         if(root == NULL) {
             return true;
         }
             
-        bool isLeftUnival = countUnivalSubtreesRecursive(root->left, count);
-        bool isRightUnival = countUnivalSubtreesRecursive(root->right, count);
-            
-        if (isLeftUnival && isRightUnival
-            && ((root->left == NULL) || root->left->val == root->val)
-            && ((root->right == NULL) || root->right->val == root->val)
-        ) {
+        bool is_left_unival = count_unival_subtrees_recursive(root->left, count);
+        bool is_right_unival = count_unival_subtrees_recursive(root->right, count);
+        
+        // leaf까지와서 (좌 우 null), 체크하거나 혹은 노드의 좌우가 값이 노드의 값이랑 일치하는지 판단
+        if (is_left_unival && is_right_unival && ((root->left == NULL) || root->left->val == root->val) && ((root->right == NULL) || root->right->val == root->val)) {
             ++count;
             return true;
         }
@@ -23,7 +21,7 @@ bool countUnivalSubtreesRecursive(TreeNode* root, int& count)
     int countUnivalSubtrees(TreeNode* root) 
     {
         int count = 0;
-        countUnivalSubtreesRecursive(root, count);
+        count_unival_subtrees_recursive(root, count);
         return count;
     }
 };
