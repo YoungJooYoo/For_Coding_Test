2c6541a8eb48e591acf9f6309d9b7a8403768397
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-17 07:53:32 +0900

    Time: 3 ms (89.03%), Space: 16.4 MB (61.35%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.cpp
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.cpp
@@ -1,26 +1,29 @@
-   class Solution {
-    public:
-        int countUnivalSubtrees(TreeNode* root) {
-            int count = 0;
-            countUnivalSubtreesRecursive(root, count);
-            return count;
+class Solution {
+public:
+bool countUnivalSubtreesRecursive(TreeNode* root, int& count)
+    {
+        if(root == NULL) {
+            return true;
         }
-        
-        bool countUnivalSubtreesRecursive(TreeNode* root, int& count)
-        {
-            if(root == NULL) return true;
             
-            auto isLeftUnival = countUnivalSubtreesRecursive(root->left, count);
-            auto isRightUnival = countUnivalSubtreesRecursive(root->right, count);
+        bool isLeftUnival = countUnivalSubtreesRecursive(root->left, count);
+        bool isRightUnival = countUnivalSubtreesRecursive(root->right, count);
             
-            if(isLeftUnival && isRightUnival
-                && ((root->left == NULL) || root->left->val == root->val)
-                && ((root->right == NULL) || root->right->val == root->val)
-            )
-            {
-                ++count;
-                return true;
-            }
-            return false;
+        if (isLeftUnival && isRightUnival
+            && ((root->left == NULL) || root->left->val == root->val)
+            && ((root->right == NULL) || root->right->val == root->val)
+        ) {
+            ++count;
+            return true;
         }
-    };
+    
+        return false;
+    }
+    
+    int countUnivalSubtrees(TreeNode* root) 
+    {
+        int count = 0;
+        countUnivalSubtreesRecursive(root, count);
+        return count;
+    }
+};
