a45779029de895cadc051738894720b9794c4e57
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-17 22:02:11 +0900

    Time: 7 ms (73.72%), Space: 7.8 MB (5.07%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool helper(string s, string t) 
+    {
+        unordered_map<char, char> mp;
+        //map<char, char> mp;
+        
+        for (size_t i = 0; i < s.size(); i++) {
+            if (mp.find(s[i]) != mp.end()) {  // s[i]가 이미 존재한다면,
+                if (mp[s[i]] != t[i]) { // 해당 문자가 존재하는 상태에서 t문자여르이 문자와 일치 하지 않으면 반환
+                    return false;
+                }
+            }
+            else {
+                mp[s[i]] = t[i];
+            }
+        }
+        
+        return true;
+    }
+    
+    bool isIsomorphic(string s, string t) 
+    {
+        cout << helper(s, t) << endl;
+        cout << helper(t, s) << endl;
+        return (helper(s, t) && helper(t, s)) ? true : false;
+    }
+};
