1a3cef992089aefc95ef1d2af2eb47ddebdb6894
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-17 09:18:27 +0900

    Time: 11 ms (32.19%), Space: 12.3 MB (19.40%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -2,30 +2,11 @@ class Solution {
 public:
     int findMin(vector<int>& nums) 
     {
-        int low = 0;
-        int high = nums.size()-1;
-        
-        while (low <= high) { // or low < high
-            int mid = low + (high - low) / 2;
-            
-            if (nums[mid] < nums[high]) {
-                high = mid;
-            }
-            else if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            }
-            else { // nums[mid] == nums[high]
-                high--;
-            }
+        if (nums.size() == 0) {
+            return 0;
         }
-        return nums[low];
+        
+        sort(begin(nums), end(nums));
+        return nums[0];
     }
 };
-
-/*
-풀이법 :
-
-정렬되어 있는 배열에서 최소 값을 찾는 것이므로
-이진탐색을 진행한다.
-
-*/
