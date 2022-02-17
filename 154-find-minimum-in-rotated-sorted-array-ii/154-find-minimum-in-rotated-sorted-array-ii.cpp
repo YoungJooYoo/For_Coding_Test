a6221b84fdea5195f99f23ab38552b0d05619a94
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-17 09:07:07 +0900

    Time: 0 ms (100.00%), Space: 12.3 MB (19.40%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -5,18 +5,21 @@ public:
         int low=0;
         int high=nums.size()-1;
         
-        while(low<high){
+        while(low<=high){
             int mid=low+(high-low)/2;
-            cout << mid << endl;
+            cout << "mid : " <<mid << endl;
             
             if(nums[mid]<nums[high]){
                 high=mid;
+                cout << "high : " <<high << endl;
             }
             else if(nums[mid]>nums[high]){
-                low=mid+1;
+                low=mid + 1;
+                cout << "low : " << low << endl;
             }
             else{
                 high--;
+                cout << "high-- : " << high << endl;
             }
         }
         return nums[low];
