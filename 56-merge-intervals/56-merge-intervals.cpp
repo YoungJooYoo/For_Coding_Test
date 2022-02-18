b20f4f899ffeb58f094eda9a437ac9133a98c992
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 00:49:36 +0900

    Time: 47 ms (47.02%), Space: 19.1 MB (46.40%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+	{
+		vector<vector<int>> result;
+
+		sort(intervals.begin(), intervals.end());
+
+		for (const auto& interval : intervals) {
+			// result를 다음과 같이 가정하면 [1, 2], [3, 4]
+			// if문이 1과 3을 채우고, else 문이 2,4를 채운다.
+			if (result.empty() == true || result.back()[1] < interval[0]) {
+				result.push_back(interval); // [1,4] [5,6] 상황에서 [1,4]을 선택해 넣겠다.
+				// 완성본 : [1 , 6]
+			}
+			else { // result.back()[1] >= interval[0])
+				result.back()[1] = max(result.back()[1], interval[1]); // [1,4] [5,6] 중 4,6 둘에서 큰거 선택하겠다
+			}
+		}
+		
+		return result;
+    }
+};
