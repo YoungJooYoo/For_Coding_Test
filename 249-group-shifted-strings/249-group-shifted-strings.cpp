0f2451b9465bd8955c30dac64bb4c7b82ebfc118
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-18 15:34:03 +0900

    Time: 10 ms (24.81%), Space: 8 MB (73.41%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.cpp
+++ 249-group-shifted-strings/249-group-shifted-strings.cpp
@@ -4,15 +4,15 @@ public:
         vector<vector<string> > res;
         unordered_map<string, vector<string>> m;
         
-        for (auto& str : strings) {
+        for (const auto& str : strings) {
             string k = "";
-            for (char c : str)  {
+            for (const char& c : str)  {
                 k += (c + 26 - str[0]) % 26 + 'a'; // encoded with lower  case letters: ensure a[0] is mapped to be 'a'; "+ 26" means to avoid to handle negative number
             }
             m[k].push_back(str);            
         }
         
-        for(auto& [k, v]: m) { 
+        for(const auto& [k, v]: m) { 
             res.emplace_back(v);
         }
         
