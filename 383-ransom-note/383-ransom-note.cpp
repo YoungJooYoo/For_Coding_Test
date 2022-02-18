3d3d91cdeeb074db3bb72d8fc77e276275ccff32
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 00:50:32 +0900

    Time: 33 ms (24.79%), Space: 8.9 MB (41.86%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        unordered_map<char, int> mp;
+        
+        // magazine 길이가 더 기므로 먼저 시작
+        for (char& c : magazine) {
+            mp[c]++;
+        }
+        for (char& c : ransomNote) {
+            if (mp[c] == 0) { // 매거진이 쓰이지 않았다 == 0
+                return false;
+            }
+            mp[c]--;
+        }
+    
+        return true;
+    }
+};
