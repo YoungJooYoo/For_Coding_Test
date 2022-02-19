fdab926f3c357122ef1dcad5e0f40efdba479817
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 22:37:57 +0900

    Time: 8 ms (55.72%), Space: 15.6 MB (11.55%) - LeetHub

--- 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
+++ 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -33,7 +33,7 @@ public:
             x = 0;
         }
         if (y == n) { // 열의 끝값에 도착한 경우
-            return isZeroMat(mat) ? 0 : 10000; // 왜?? 100000
+            return isZeroMat(mat) ? 0 : 100; // 왜?? 100000
         }
 
         int ret1 = FlipOrNotFlip(mat, y, x + 1); // flip전에 flip할 범위를 
@@ -48,7 +48,7 @@ public:
         m = mat[0].size(); // x에 대응
         int ret = FlipOrNotFlip(mat, 0, 0);
         
-        return ret >= 10000 ? -1 : ret;
+        return ret >= 100 ? -1 : ret;
     }
     
 private: 
