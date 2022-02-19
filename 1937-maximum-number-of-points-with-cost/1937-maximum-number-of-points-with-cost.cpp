00b174f1533f75e7a74f42e722388a6247ca971d
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 16:19:12 +0900

    Time: 482 ms (35.59%), Space: 82.4 MB (95.09%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -12,12 +12,13 @@ public:
             for (size_t j = 0; j < col; j++) { 
                 dp[j] += points[i][j];
             }
-            for (size_t j = 1; j < col; j++){
-                dp[j] = max(dp[j], dp[j - 1] - 1);
-            }
             for (int j = col - 2; j >= 0; j--) {
                 dp[j] = max(dp[j], dp[j + 1] - 1);
             }
+            for (size_t j = 1; j < col; j++){
+                dp[j] = max(dp[j], dp[j - 1] - 1);
+            }
+            
         }
     
         for (size_t i = 0; i < col; i++) {
