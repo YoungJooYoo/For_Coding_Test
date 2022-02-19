81de6b16fe85e5992cf7f03c29a87cbda88dd2d7
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 18:36:35 +0900

    Time: 109 ms (12.08%), Space: 24.6 MB (14.71%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
@@ -3,7 +3,7 @@ public:
     bool removeOnes(vector<vector<int>>& grid) 
     {
         for (size_t i = 0; i < grid.size(); ++i) {
-            for (size_t j = 0; j < grid[i].size(); ++j) {
+            for (size_t j = 0; j < grid[0].size(); ++j) {
                 if (grid[i][j] ^ grid[i][0] ^ grid[0][j] ^ grid[0][0]) {
                     return false;
                 }
