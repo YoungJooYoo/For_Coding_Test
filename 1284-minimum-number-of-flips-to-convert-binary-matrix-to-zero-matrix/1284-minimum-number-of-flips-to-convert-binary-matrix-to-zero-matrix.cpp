138df5867f44fe1dbf2c400ae5f0f47b76964f34
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 21:29:22 +0900

    Time: 11 ms (47.23%), Space: 15.5 MB (11.55%) - LeetHub

--- 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
+++ 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -4,29 +4,40 @@ public:
     vector<vector<int>>& flip(vector<vector<int>>& mat, int y, int x) 
     {
         mat[y][x] ^= 1;
-        if(y - 1 >= 0) mat[y-1][x] ^= 1;
-        if(y + 1 <  n) mat[y+1][x] ^= 1;
-        if(x - 1 >= 0) mat[y][x-1] ^= 1;
-        if(x + 1 <  m) mat[y][x+1] ^= 1;
+        
+        if (y - 1 >= 0) mat[y-1][x] ^= 1;
+        if (y + 1 <  n) mat[y+1][x] ^= 1;
+        if (x - 1 >= 0) mat[y][x-1] ^= 1;
+        if (x + 1 <  m) mat[y][x+1] ^= 1;
+        
         return mat;
     }
 
     bool isZeroMat(vector<vector<int>>& mat) 
     {
-        for(auto i = 0; i < n; ++i)
-            for(auto j = 0; j < m; ++j)
-                if(mat[i][j])
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = 0; j < m; ++j){ 
+                if (mat[i][j]) {
                     return false;
+                }
+            }
+        }
+        
         return true;
     }
 
     int FlipOrNotFlip(vector<vector<int>> mat, int y, int x) 
     {
-        if (x == m) y++, x = 0;
-        if (y == n) return isZeroMat(mat) ? 0:10000;
+        if (x == m) {
+            y++, x = 0;
+        }
+        if (y == n) {
+            return isZeroMat(mat) ? 0:10000;
+        }
 
-        auto ret1 = FlipOrNotFlip(mat, y, x+1);
-        auto ret2 = FlipOrNotFlip(flip(mat, y, x), y, x+1) + 1;
+        int ret1 = FlipOrNotFlip(mat, y, x+1);
+        int ret2 = FlipOrNotFlip(flip(mat, y, x), y, x+1) + 1;
+        
         return min(ret1, ret2);
     }
 
