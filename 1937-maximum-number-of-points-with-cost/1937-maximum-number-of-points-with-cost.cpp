e56cdb3a86247fe352cf84e7a3e0c2d1f4a9990b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-19 14:51:15 +0900

    Time: 418 ms (49.94%), Space: 82.2 MB (97.43%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -7,21 +7,22 @@ public:
         int row = points.size();
         int col = points[0].size();
         long long temp[col];
-        
-        for(i = 0; i< col; i++) {
-            temp[i] = points[row - 1][i];
-        }
-            
         long long ans = 0;
         
+        // 주어진 배열의 마지막 행(마지막 줄) 값을 temp로 가져온다.
+        for(i = 0; i < col; i++) {
+            temp[i] = points[row - 1][i];
+        }
+        
+        // 밑에서 윗줄부터 시작한다.
         for (i = row - 2; i >= 0; i--) {
             long long p1 = INT_MIN;
             long long p2 = p1;
             for (j = 0; j < col; j++) {
-                p1 = max(p1, temp[j] + j);
+                p1 = max(p1, temp[j] + j); // temp 이전줄 값을 사용해 더한다.
                 temp[j] = p1 - j;
             }
-            for(j = col - 1; j >= 0; j--){
+            for (j = col - 1; j >= 0; j--){
                 p2 = max(p2, temp[j] - j);
                 temp[j] = max(temp[j], p2 + j);
                 temp[j] += points[i][j];
