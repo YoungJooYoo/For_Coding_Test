bac0a25e65963edcd1258548090e06e55305e95f
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-20 11:14:53 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (96.40%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -5,6 +5,8 @@ public:
         bool rgb_check[10][3] = {0, }; // init false;  기둥 - RGB로 매핑;
         int ans = 0;
         
+        // rings에서 i = 1, 3, 5, 7 홀수로 나가야지 기둥을 선택할 수 있다.
+        // i - 1은,  기둥 인덱스 1, 3, 5, 7 이전의 값인 0, 2, 4, 6인덱스에 RGB값이 있다.
         for (size_t i = 1; i < rings.size(); i += 2) {
             if (rings[i - 1] == 'R') {
                 rgb_check[rings[i] - '0'][0] = true;
@@ -16,6 +18,7 @@ public:
                 rgb_check[rings[i]-'0'][2] = true;
             }
         }
+        
         for (size_t i = 0; i < 10; i++) {
             ans += rgb_check[i][0] && rgb_check[i][1] && rgb_check[i][2];
         }
