342c1eafbfea79895cbdf80dd886e776f51974a7
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-26 18:29:58 +0900

    Time: 2 ms (39.17%), Space: 6 MB (73.77%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,38 +1,49 @@
 class Solution {
 public:
-    string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int , char>> pq;
-        if(a> 0)                         // Pushing all non-zero occurances in queue
-        pq.push({a, 'a'});
-        if(b>0)
-        pq.push({b, 'b'});
-        if(c>0)
-        pq.push({c, 'c'});
-        string ans = "";
-        int size = 0;                    //size of resultant string
-        while(!pq.empty()){
-            auto it = pq.top();          //pop character with higest occurance
+    string longestDiverseString(int a, int b, int c) 
+    {
+        priority_queue<pair<int, char>> pq;
+        string result = "";
+        
+        /* 알파벳 갯수 - 알파벳 매핑 */
+        if(a > 0) {                      
+            pq.push({a, 'a'});
+        }
+        if (b > 0) {
+            pq.push({b, 'b'});
+        }
+        if (c > 0) {
+            pq.push({c, 'c'});
+        }
+        
+        int size = 0;
+        
+        while(!pq.empty()) {
+            auto it = pq.top();
             pq.pop();
-            if(size>1 && ans[size-1] == it.second && ans[size-2] == it.second){  //if there are already two consecutive occurances hold that character and pop the next character and put it 
-                if(pq.empty())return ans;
-                ans += pq.top().second;
-                //cout<<ans<<"\n";
+            if (size > 1 && result[size - 1] == it.second && result[size-2] == it.second) { 
+                if (pq.empty()) {
+                    return result;
+                }
+                result += pq.top().second;
                 auto tp = pq.top();
                 size++;
                 pq.pop();
-                if(tp.first >1)
-                pq.push({tp.first-1, tp.second});               // push the characters back into queue
+                if( tp.first >1) {
+                    pq.push({tp.first-1, tp.second});           
+                }
                 pq.push({it.first, it.second});
             }
-            else {                                   //otherwise put the character and push it again.
-                ans += it.second;
+            else {                                  
+                result += it.second;
                 size++;
-                if(it.first > 1)
-                pq.push({it.first-1, it.second});
-            }
-            
+                if(it.first > 1) {
+                    pq.push({it.first-1, it.second});
+                }
+            }   
         }
-        return ans;
         
+        
+        return result;
     }
 };
