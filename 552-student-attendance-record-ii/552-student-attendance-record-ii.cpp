bb0433b59dca89b4a6b0909154d609f0da23742b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-26 16:39:05 +0900

    Time: 75 ms (69.31%), Space: 7.6 MB (67.82%) - LeetHub

--- 552-student-attendance-record-ii/552-student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/552-student-attendance-record-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    long M = 1000000007;
-    
-    int checkRecord(int n) 
-    {
+    int checkRecord(int n) {
         long long f[1000000 + 1];
         f[0] = 1;
         f[1] = 2;
         f[2] = 4;
         f[3] = 7;
         
-        for (int i = 4; i <= n; i++)
+        for (size_t i = 4; i <= n; i++) {
             f[i] = ((2 * f[i - 1]) % M + (M - f[i - 4])) % M;
+        }
         
         long long sum = f[n];
         
-        for (int i = 1; i <= n; i++) {
+        for (size_t i = 1; i <= n; i++) {
             sum += (f[i - 1] * f[n - i]) % M;
         }
         
         return (int)(sum % M);
     }
+    
+private:
+    long M = 1000000007;
 };
