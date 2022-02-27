a00c5ee1c4c7c89d1314af1da3e796adb8570551
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-27 12:55:21 +0900

    Time: 337 ms (58.81%), Space: 101 MB (44.11%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -3,18 +3,18 @@ public:
     SnapshotArray(int length) {}
     
     void set(int index, int val) {
-        mp[snap_index][index] = val;
+        mp[mp_snap_index][index] = val;
         
     }
     
     int snap() {
-        snap_index++;
-        return snap_index - 1;
+        mp_snap_index++; 
+        return mp_snap_index - 1;
     }
     
     int get(int index, int snap_id) {
         for (int i = snap_id; i >= 0; i--) {
-            if (mp[i].count(index) == 1)
+            if (mp[i].count(index) == 1) // index 존재 유무 확인
                 return mp[i][index];
         }
         
@@ -22,6 +22,6 @@ public:
     }
     
 private:
-    unordered_map<int, unordered_map<int, int>> mp;
-    int snap_index = 0;
+    unordered_map<int, unordered_map<int, int>> mp; // 변경사항을 저장하기 위한 해시
+    int mp_snap_index = 0; // mp 전용 인덱스
 };
