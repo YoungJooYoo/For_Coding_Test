6c183bf156954d2b3e9af3bb070bbacdd65e859c
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-27 17:01:37 +0900

    Time: 4 ms (75.55%), Space: 6.5 MB (95.09%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
@@ -8,23 +8,18 @@ public:
         int n = s.size();
         
         for (int i = 0; i <= n - k; i++) {
-            // Initializing an empty frequency array
             int freq[26] = {0};
             bool isUnique = true;
             
             for (int j = i; j < i + k; j++) {
-                // Incrementing the frequency of current character
                 freq[s[j] - 'a']++;
                 
-                // If a repeated character is found, we stop the loop
                 if (freq[s[j] - 'a'] > 1) {
                     isUnique = false;
                     break;
                 }
             }
             
-            // If the substring does not have any repeated characters,
-            // we increment the answer
             if (isUnique) {
                 answer++;
             }
