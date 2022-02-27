cecde072adcc9cd01fb5b2672e77c22ea6fe91b4
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-27 16:01:19 +0900

    Time: 4 ms (45.12%), Space: 7.2 MB (83.66%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.cpp
+++ 362-design-hit-counter/362-design-hit-counter.cpp
@@ -12,7 +12,7 @@ public:
         int count = 0;
         int time_stamp_300 = timestamp - 300;
         for (int i = time_stamp_log.size() - 1; i >= 0; i--) {
-            if (time_stamp_log[i] <= time_stamp_300 ) {
+            if (time_stamp_log[i] <= time_stamp_300) {
                 return count;
             }
             count++;
