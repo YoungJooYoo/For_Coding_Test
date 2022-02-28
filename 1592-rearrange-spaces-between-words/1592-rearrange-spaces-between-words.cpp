a0990091f5cb4702c9e356a07a242ebe5555cea9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-01 00:20:25 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (15.36%) - LeetHub

---
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reorderSpaces(string text) 
+    {
+        int spaces = 0;
+        string word;
+        string s;
+        vector<string> words;
+        
+        for (const auto& ch : text) {
+            if (ch == ' ') {
+                spaces++;
+                if (!word.empty())
+                    words.push_back(word);
+                word = "";
+            }
+            else
+                word += ch;
+        }
+        
+        if (!word.empty()) {
+            words.push_back(word);
+        }
+        if (words.size() == 1) {
+            return words.back() + string(spaces, ' ');
+        }
+        
+        int gap = spaces / (words.size() - 1);
+        int suff = spaces % (words.size() - 1);
+        
+        for (auto w : words) {
+            s += w + string(gap, ' ');
+        }
+        
+        return s.substr(0, s.size() - gap) + string(suff, ' ');
+    }
+};
