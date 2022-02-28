65258f3f57c4bd5396bbcbf85c04bc1b22eb3f33
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-02-28 17:24:54 +0900

    Time: 18 ms (50.51%), Space: 10.7 MB (89.22%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -5,26 +5,29 @@ public:
         vector<string> diglog;
         vector<pair<string, string>> letlog;
         for (const string& log : logs) {
-            int i = 0;
+            size_t i = 0;
             while (log[i]!=' ') {
                 i++;
             } 
-            if (isalpha(log[i+1])) {
-                letlog.emplace_back(log.substr(0, i), log.substr(i+1));
+            if (isalpha(log[i + 1])) { //  알파뱃인지 확인
+                letlog.emplace_back(log.substr(0, i), log.substr(i + 1));
             }
             else {
                 diglog.push_back(log);
             }
         }
-        sort(letlog.begin(), letlog.end(), [&](auto& a, auto& b) //comparator to check if second part of string, i.e. the string 
-             //without identifier equal then compare identifier, or else compare second half and decide sorting on that
-             {
-                 return a.second==b.second? a.first<b.first : a.second<b.second;
-             });
+        sort(letlog.begin(), letlog.end(), [&](auto& a, auto& b) {
+            return a.second==b.second? a.first<b.first : a.second<b.second;
+        });
         
         vector<string> ret;
-        for(auto &p:letlog) ret.push_back(p.first + " " + p.second);
-        for(string &s:diglog) ret.push_back(s);
+        for(const auto& p : letlog) {
+            ret.push_back(p.first + " " + p.second);
+        }
+        for(const string& s : diglog) {
+            ret.push_back(s);
+        }
+        
         return ret;
     }
 };
