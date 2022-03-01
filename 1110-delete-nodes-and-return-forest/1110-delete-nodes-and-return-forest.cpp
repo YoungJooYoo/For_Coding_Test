e9bac27468b447a221de0f2f356ac14ecb832c38
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-02 00:04:06 +0900

    Time: 24 ms (82.10%), Space: 25.2 MB (89.45%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -10,6 +10,7 @@ public:
         dfs(root->left,to_delete);
         dfs(root->right,to_delete);
         
+        // to_delete 배열에 root->val 값이 있는지 확인한다. 없다면 end를 반환
         if (find(to_delete.begin(), to_delete.end(), root->val) != to_delete.end()) {
             if (root->left) {
                 res.push_back(root->left);
