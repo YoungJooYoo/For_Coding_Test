078c706e590c78f39046b8c9031b8ae4c54ba610
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-02 00:08:28 +0900

    Time: 28 ms (69.46%), Space: 25.3 MB (79.43%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    
-    void dfs(TreeNode* &root,vector<int>& to_delete)
+    void dfs(TreeNode* &root, vector<int>& to_delete) 
     {
         if (root == nullptr) {
             return;
-        }
+        } 
         
-        dfs(root->left,to_delete);
-        dfs(root->right,to_delete);
+        dfs(root->left, to_delete);
+        dfs(root->right, to_delete);
         
         // to_delete 배열에 root->val 값이 있는지 확인한다. 없다면 end를 반환
         if (find(to_delete.begin(), to_delete.end(), root->val) != to_delete.end()) {
@@ -18,16 +17,16 @@ public:
             if (root->right) {
                 result.push_back(root->right);
             }
-            
             root = nullptr;
             delete root;
+                
         }
+        
     }
     
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) 
     {
- 
-        dfs(root,to_delete);
+        dfs(root, to_delete);
         
         if (root) {
             result.push_back(root);
@@ -37,5 +36,5 @@ public:
     }
     
 private:
-    vector<TreeNode*> result;
+   vector<TreeNode*> result;
 };
