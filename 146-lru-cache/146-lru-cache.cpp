8d9be85d14c4e8c8d5afc51b3fd8ffd75f9938e8
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-02 16:42:56 +0900

    Time: 400 ms (90.61%), Space: 164.2 MB (95.98%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -34,10 +34,3 @@ private:
     queue <int> q;
     size_t cache_capacity;
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
