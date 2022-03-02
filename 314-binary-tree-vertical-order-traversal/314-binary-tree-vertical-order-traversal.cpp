8cb6952cd1aba6d7165d1da512725ca8b54a50ea
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-02 20:08:59 +0900

    Time: 3 ms (89.57%), Space: 12.4 MB (61.22%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
@@ -11,8 +11,7 @@ public:
         q.push({root, 0});
         
         while (!q.empty()) {
-            pair<TreeNode*, int> temp = q.front();
-            q.pop();
+            pair<TreeNode*, int> temp = q.front(); q.pop();
             m[temp.second].push_back(temp.first->val);// put the current element into its group in the map
             
             if(temp.first->left) {
@@ -34,7 +33,7 @@ public:
         BFS(root, m);
         
         // map안에 저장된 배열을 순차적으로 ans 배열에 집어 넣는다.
-        for(const auto& it : m){
+        for (const auto& it : m) {
             ans.push_back(it.second);
         }
         
