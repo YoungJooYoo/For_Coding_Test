2992cbc3fbb2a2b278a633ddff38ea765a282471
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-02 16:47:42 +0900

    Time: 801 ms (10.98%), Space: 164.2 MB (95.54%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -22,15 +22,15 @@ public:
         
         while (cache.size() > cache_capacity) {
             int cur = q.front(); q.pop(); // LRU 특성상 앞부분부터 제거
-            if ((cache[cur]--) == 1) {
+            if ((cache[cur]--) == 1) { // 호출 1번밖에 안된 cache를 제거
                 cache.erase(cur);
             }
         }
     }
     
 private:
-    unordered_map <int, int> m; // key - value 자료구조 
-    unordered_map <int, int> cache; // cache사이즈가 일정하게 유지
-    queue <int> q;
+    unordered_map <int, int> m; // key - value 자료구조, 들어오는 모든 inpput 저장
+    unordered_map <int, int> cache; // cache사이즈가 일정하게 유지, 호출된 횟수기록 빈도기록
+    queue <int> q; // get or put 호출한 순서를 기록
     size_t cache_capacity;
 };
