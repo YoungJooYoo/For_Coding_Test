f16cf6709d0fd261e5afa5743112a8e7754a7d08
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-02 16:42:51 +0900

    Time: 408 ms (88.22%), Space: 164.1 MB (96.24%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,43 @@
+class LRUCache {
+public:
+    
+    LRUCache(int capacity) : cache_capacity(capacity) {}
+    
+    int get(int key) 
+    {
+        if (cache.find(key) == cache.end()) {
+            return -1;
+        }
+        q.push(key);
+        cache[key]++;
+        
+        return m[key];
+    }
+    
+    void put(int key, int value) 
+    {
+        q.push(key);
+        cache[key]++;
+        m[key] = value;
+        
+        while (cache.size() > cache_capacity) {
+            int cur = q.front(); q.pop(); // LRU 특성상 앞부분부터 제거
+            if ((cache[cur]--) == 1) {
+                cache.erase(cur);
+            }
+        }
+    }
+    
+private:
+    unordered_map <int, int> m; // key - value 자료구조 
+    unordered_map <int, int> cache; // cache사이즈가 일정하게 유지
+    queue <int> q;
+    size_t cache_capacity;
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
