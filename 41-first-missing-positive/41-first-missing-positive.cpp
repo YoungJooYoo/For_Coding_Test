fa5dd54c52ad4055964c12acc3583dce65881e6b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-06 15:50:12 +0900

    Time: 336 ms (22.29%), Space: 82.9 MB (79.44%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -5,8 +5,9 @@ public:
         const size_t n = nums.size();
         
         for (size_t i = 0 ; i < n; i++) {
-            while (nums[i] > 0 and nums[i] <= n and nums[i] != nums[nums[i] - 1]) // 양수만 신경쓰기, 중복 제거하기
+            while (nums[i] > 0 and nums[i] <= n and nums[i] != nums[nums[i] - 1]) { // 양수만 신경쓰기, 중복 제거하기
                 swap(nums[i], nums[nums[i] - 1]);
+            }
         }
         
         for (size_t i = 0 ; i < n ; i++) {
