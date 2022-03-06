440abd9b252d3e545c76316b00fb5fed3fa47d9a
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-06 16:19:56 +0900

    Time: 136 ms (88.48%), Space: 68.7 MB (88.18%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -4,19 +4,19 @@ public:
     {
         int rightMaxHeight = 0; // tallest building to the right of current index
         int i = heights.size() - 1; // i: current index
-        vector<int> buildingIdx; // buildingIdx: the result vector to be returned
+        vector<int> result; 
         
         /* 배열의 맨 뒤에서부터 작은 건물 순서대로 접근을 시작한다. */
         while (i >= 0) { // loop starts from the very end
             if (rightMaxHeight < heights[i]) { // 뒤에서부터 출발해 점점 건물이 커지는 조건이 맞다면 정답배열에 넣는다. 
-                buildingIdx.push_back(i);
+                result.push_back(i);
             }
             rightMaxHeight = max(rightMaxHeight, heights[i]); // update tallest building to the right to include current building
             i--; //decrease i by 1 to move left
         }
         
-        reverse(buildingIdx.begin(), buildingIdx.end()); // reverse the building idices as we need it to be in increasing order
+        reverse(result.begin(), result.end()); // reverse the building idices as we need it to be in increasing order
     
-        return buildingIdx;
+        return result;
     }
 };
