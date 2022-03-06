fd88c2a419b1752cbdd48351b4815cfa2e20949b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-06 17:20:29 +0900

    Time: 16 ms (70.08%), Space: 10.3 MB (64.67%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -5,14 +5,11 @@ public:
         Node* a = p;
         Node* b = q;
         
-        size_t count = 0;
         while(a != b) {
             a = (a == nullptr) ? q : a->parent; // p or q 둘다 됨
             b = (b == nullptr) ? p : b->parent; // p or q둘다됨   //  q, p순서가 더 빠르다.
-            count++;
         }
-                 
-        cout << "count : " << count << endl;
+
         return b; // a or b 둘다 됨
     }
 };
