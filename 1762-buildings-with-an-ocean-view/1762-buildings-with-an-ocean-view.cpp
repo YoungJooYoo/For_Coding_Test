8b6c7c6e7f1616b4d118ed1981d26458c85edf5c
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-06 16:16:37 +0900

    Time: 116 ms (99.40%), Space: 68.8 MB (70.26%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    vector<int> findBuildings(vector<int>& heights) {
+    vector<int> findBuildings(vector<int>& heights) 
+    {
+        int rightMaxHeight = 0; // tallest building to the right of current index
+        int i = heights.size() - 1; // i: current index
+        vector<int> buildingIdx; // buildingIdx: the result vector to be returned
         
-        // tallest building to the right of current index
-        int rightMaxHeight = 0;
-        
-        // i: current index
-        int i = heights.size()-1;
-        
-        // buildingIdx: the result vector to be returned
-        vector<int> buildingIdx;
-        
-        // loop starts from the very end
-        while(i >= 0)
-        {
-            // if the tallest building to the right of current i is shorter
-            if(rightMaxHeight < heights[i])
+        while (i >= 0) { // loop starts from the very end
+            if (rightMaxHeight < heights[i]) {// if the tallest building to the right of current i is shorter
                 buildingIdx.push_back(i);
-            
-            // update tallest building to the right to include current building
-            rightMaxHeight = max(rightMaxHeight, heights[i]);
-            
-            //decrease i by 1 to move left
-            i--;
+            }
+            rightMaxHeight = max(rightMaxHeight, heights[i]); // update tallest building to the right to include current building
+            i--; //decrease i by 1 to move left
         }
         
-        // reverse the building idices as we need it to be in increasing order
-        reverse(buildingIdx.begin(), buildingIdx.end());
+        reverse(buildingIdx.begin(), buildingIdx.end()); // reverse the building idices as we need it to be in increasing order
     
         return buildingIdx;
     }
