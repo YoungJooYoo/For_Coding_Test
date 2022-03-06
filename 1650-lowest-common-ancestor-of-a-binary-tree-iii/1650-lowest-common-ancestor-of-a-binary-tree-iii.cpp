ffa37adc8ae84d6bc042194418d90b4e249238cc
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-06 17:05:24 +0900

    Time: 17 ms (62.71%), Space: 10 MB (95.97%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -1,39 +1,14 @@
 class Solution {
 public:
-    Node* lowestCommonAncestor(Node* p, Node * q) 
-    {
-		// Check if any of p,q can be LCA.
-        if(subtree(p, q) == true) {
-            return p;
-        }
-        if(subtree(q, p) == true) {
-            return q;
-        }
-        // It means LCA is in the upper tree.
-        return upperTree(p->parent, p, q);
-    }
-    
-    bool subtree(Node* root, Node * target) // 주어진 p, q인지 확인한다.
-    {
-        if (root == nullptr) {
-            return false;
-        }
-        if (root == target)  {
-            return true;
-        }
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        Node* a = p;
+        Node* b = q;
         
-        return subtree(root->left, target) || subtree(root->right, target);
-    }
-    
-    Node* upperTree(Node* parent, Node* child, Node* target) 
-    {
-        if (parent->right == child && subtree(parent->left, target)) {
-            return parent;
+        while(a != b) {
+            a = (a == nullptr) ? q : a->parent;
+            b = (b == nullptr) ? p : b->parent;  
         }
-        if (parent->left == child && subtree(parent->right, target)) {
-            return parent;
-        }
-        
-        return upperTree(parent->parent, parent, target);
+                 
+        return a;
     }
 };
