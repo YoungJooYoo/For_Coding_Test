3d5b965199209bda55cf2d4bc29e0bde1ceecfcf
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-09 01:15:48 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (49.33%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) 
+	{
+        stack<char> st;
+		
+		for (const char& ch : s) {
+			if (ch == '(') {
+				st.push(ch);
+			}
+			else {
+				if (!st.empty() && st.top() == '(') {
+					st.pop();
+				}
+				else {
+					st.push(ch);
+				}
+			}
+		}
+
+		return st.size();
+    }
+};
