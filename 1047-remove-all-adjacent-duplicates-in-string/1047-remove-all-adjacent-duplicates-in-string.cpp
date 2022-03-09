496783b1814d87b8c431beedb1747ab472210f21
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-09 18:03:38 +0900

    Time: 34 ms (45.11%), Space: 11.6 MB (26.75%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -5,25 +5,25 @@ public:
         stack<char> st;
         string result = "";
         
-	    for (const char& ch : s) {
+        for (const char& ch : s) {
             if (st.empty()) {
                 st.push(ch);
             }
-            else if (st.top() == ch) {
+		    else if (st.top() == ch) {
                 st.pop();
             }
-            else {
+		    else {
                 st.push(ch);
             }
         }
-
-        while(!st.empty()) {
+        
+        while (!st.empty()) {
             result += st.top();
             st.pop();
         }
-
-	    reverse(result.begin(), result.end());
-
-	    return result;
+        
+        reverse(result.begin(), result.end());
+        
+        return result;
     }
 };
