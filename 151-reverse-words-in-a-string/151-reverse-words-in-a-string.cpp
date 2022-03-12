48d50fe70259398969a4750819794a932c8ce204
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-12 17:33:31 +0900

    Time: 8 ms (52.47%), Space: 20.8 MB (18.61%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -16,7 +16,9 @@ public:
                 break; // 문자열 끝까지 갔다면 종료
             }
             j = i + 1;
-            while (j < len && s[j] != ' ') j++; // 공백이 아니면 인덱스 전진
+            while (j < len && s[j] != ' ') {
+                j++; // 공백이 아니면 인덱스 전진
+            }
             tmp = s.substr(i, j - i); // 단어별로 서브스트링 생성
             res = tmp + (res.size() ? " " : "") + res;
             // moving to the next word
