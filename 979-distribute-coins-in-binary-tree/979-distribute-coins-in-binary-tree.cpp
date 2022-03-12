7548d6795110177be4eb7539323ffea2d4dbf0ab
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-12 18:00:10 +0900

    Time: 9 ms (44.21%), Space: 13.9 MB (46.61%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int distributeCoins(TreeNode* root) 
+    {   
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+    
+private:
+    int dfs(TreeNode* root)
+    {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        ans = ans + abs(left) + abs(right);
+        
+        return root->val + left + right - 1;
+    }
+    
+    int ans;
+};
