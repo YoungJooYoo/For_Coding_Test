6e80307a1228e1ee776c16887ef301393ae86593
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-13 00:18:00 +0900

    Time: 36 ms (89.94%), Space: 56.7 MB (34.52%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    vector<TreeNode*> ans;
-      unordered_map<string, int>mymap;
-  string helper(TreeNode* root)
+    string helper(TreeNode* root)
     {
-        if(root == NULL) return "";
+        if (root == nullptr) {
+            return "";
+        }
         
-        string l= helper(root->left);
-        string r= helper(root->right);
-        string s = "(" + l + to_string(root->val) + r + ")"; //make unique string of each subtree
-        if(mymap[s]!=-1) mymap[s]++; // if string is not present insert and increase count
-      
-      if(mymap[s]>1) // if same string found more than one time we got same subtrees
-      {
-          ans.push_back(root);
-          mymap[s]=-1; // no need to insert again
-      }
-      return s; 
+        /* 오른쪽 왼쪽 dfs로 탐색하면서 서브 트리를 만들어간다. */
+        string l = helper(root->left);
+        string r = helper(root->right);
+        string s = "(" + l + to_string(root->val) + r + ")"; //make unique string of each subtree 
+        
+        if (mp[s] != -1) { // if string is not present insert and increase count
+            mp[s]++;
+        }
+        if (mp[s] > 1) { // if same string found more than one time we got same subtrees
+            ans.push_back(root);
+            mp[s] = -1; // no need to insert again
+        }
+        
+        return s; 
     }
-    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-      helper(root);
+    
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) 
+    {
+        helper(root);
         return ans;
     }
+    
+private:
+    vector<TreeNode*> ans; 
+    unordered_map<string, int> mp; // 문자열-빈도수 해시맵으로, 서브트리 확인, 여기에는 모든 서브트리가 들어간다.
 };
