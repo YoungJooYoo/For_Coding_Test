c6fdf248a7ea1ffaa92b823844cd9f16772a9188
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-12 17:07:29 +0900

    Time: 15 ms (26.89%), Space: 20.7 MB (25.95%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,30 +1,20 @@
 class Solution {
- public:
-    string reverseWords(string s) 
-    {
-        const size_t str_length = s.size();
-        int idx = 0;
-        
-        reverse(s.begin(), s.end()); // 문자열을 뒤집고 시작한다.
-        
-        for (size_t start = 0; start < str_length; ++start) {
-            if (s[start] != ' ') { // 시작점이 공백이면
-                if (idx != 0) { // go to the beginning of the word // 시작점이 공백이면
-                    s[idx++] = ' ';
-                }
-                
-                size_t end = start; // 단어의 끝으로 이동
-                
-                while (end < str_length && s[end] != ' ') {
-                    s[idx++] = s[end++];
-                }
-                reverse(s.begin() + idx - (end - start), s.begin() + idx); // 단어별로 다시 reverse
-                start = end; // 다음단어로 이동
-            }
+public:
+    string reverseWords(string s) {
+        string res, tmp;
+        int i = 0, j, len = s.size();
+        while (true) {
+            // advancing i and j indexes around the next word
+            while (i < len && s[i] == ' ') i++;
+            // exit condition - we are done in case
+            if (i >= len) break;
+            j = i + 1;
+            while (j < len && s[j] != ' ') j++;
+            tmp = s.substr(i, j - i);
+            res = tmp + (res.size() ? " " : "") + res;
+            // moving to the next word
+            i = j + 1;
         }
-        
-        s.erase(s.begin() + idx, s.end());
-        
-        return s;
+        return res;
     }
 };
