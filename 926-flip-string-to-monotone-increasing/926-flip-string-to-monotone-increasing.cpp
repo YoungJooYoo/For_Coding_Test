3725ca62c8a82985af178eeaf89b615fd58c4572
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-17 00:48:32 +0900

    Time: 29 ms (77.33%), Space: 9.9 MB (99.23%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -5,11 +5,12 @@ public:
         int counter_one  = 0; 
         int counter_flip = 0;
         
+        // 1을 0으로 바꿀지 0을 1로 바꿀지 고른다.
         for (const auto& ch : S) {
             if (ch == '1') {
-                ++counter_one;
+                ++counter_one; // example 3번 참조, 1을 전부 0으로 바꾸는 경우
             } else {
-                ++counter_flip;
+                ++counter_flip; // 0을 1로 바꾸는 경우
             }
             counter_flip = min(counter_one, counter_flip);
         }
