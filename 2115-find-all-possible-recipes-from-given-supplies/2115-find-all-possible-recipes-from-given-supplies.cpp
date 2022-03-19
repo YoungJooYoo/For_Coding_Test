e88ebd6c43d31f435e0aad01bf9c9028730e9aa7
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-19 21:18:51 +0900

    Time: 1325 ms (8.04%), Space: 93.3 MB (98.81%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -3,6 +3,7 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) 
     {
         unordered_set<string> items = unordered_set<string>(supplies.begin(), supplies.end());
+        // 배열값 -> 해시로 넣어서 성능 향상
         
         while(true) {
             const size_t items_size_1 = items.size();
@@ -10,31 +11,31 @@ public:
                 if (items.find(recipes[i]) != items.end()) { // 가져온 supplies에 레시피가 있는지 탐색
                     continue; // 레시피가 supplies에 있다면, continue
                 }
-                bool all_present = true; // 
+                bool is_exist_all_items = true; // 
                 for (const string& item : ingredients[i]) {
                     if (items.find(item) == items.end()) { // ingredients의 값이 supplies에 있다면, 
-                        all_present = false; 
+                        is_exist_all_items = false; 
                         break;
                     }
                 }
-                if (all_present) {
+                if (is_exist_all_items == true) {
                     items.insert(recipes[i]);
                 }
             }
             const size_t items_size_2 = items.size();
-            if (items_size_1 == items_size_2) {
+            if (items_size_1 == items_size_2) { // 만들어야하는 레시피 목록 음식과, supplies재료로 음식이 추가된 후 갯수가 서로 일치하는 경우
                 break;
             }
         }
         
-        vector<string> res;
+        vector<string> result;
         
         for (const string& recipe: recipes) {
             if (items.find(recipe) != items.end()) {
-                res.push_back(recipe);
+                result.push_back(recipe);
             }
         }
         
-        return res;
+        return result;
     }
 };
