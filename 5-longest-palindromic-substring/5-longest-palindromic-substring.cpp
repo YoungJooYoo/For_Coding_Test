2cdd13691644be713aeba4b06f1dd317a81c29e5
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-28 23:43:20 +0900

    Time: 320 ms (36.27%), Space: 11.6 MB (54.93%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -36,40 +36,49 @@ public:
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        if (s.size() <= 1) { // if size is 0 or 1 
-			return s;  // return string directly
+    string longestPalindrome(string s) 
+	{
+		const size_t str_len = s.size();
+		int max_len = 1; // the max length of palindrome in string
+        int max_index = 0;  // the first index of this max palindrome
+
+        if (str_len <= 1) {
+            return s;
 		}
-
-        int max =1; // the max length of palindrome in string
-        int f = 0;  // the first index of this max palindrome
         
-        for(int i = 0; i< s.size(); i++) { //for every element in string
-            for (int j= s.size()-1; j>=i; j--) { // check if we have the same element
-                if (s[i] == s[j]){  // if two element are same
-                    int a = i;      // check if between them is palindrome
-                    int b = j;
-                    int flag = 1;
-
-                    while(a<=b){    // from the start index i to the end index j
-                        if (s[a] == s[b]){ // if the elements between are same
-                            a++; b--;           
+        for (int i = 0; i < str_len; i++) { // for every element in string
+            for (int j = str_len - 1; j >= i; j--) { // check if we have the same element
+                if (s[i] == s[j]) {  // if two element are same
+                    int index_i = i;      // check if between them is palindrome
+                    int index_j = j;
+                    bool flag = true; // palindrome 확인 flag 일단체크, 아래 반복문에서 제대로 체크
+                    while (index_i <= index_j) {    // from the start index i to the end index j
+                        if (s[index_i] == s[index_j]){ // if the elements between are same
+                            index_i++; 
+							index_j--;           
                         }
-                        else{
-                            flag = 0; 
+                        else { // palindrome이 아니다.
+                            flag = false; 
                             break;
                         }
                     }
-
-                    if (flag ==1 && max <(j-i+1)){  // if it is palindrome and longer than max
-                        max = j-i+1;  // set it to max
-                        f = i;
+                    if (flag == true && max_len < (j - i + 1)) {  // if it is palindrome and longer than max , j - i + 1 = 길이이다.
+                        max_len = j - i + 1;  // set it to max, max 길이 갱신
+                        max_index = i; // max 길이의 시작점 갱신
                     }    
-                }
+        		}
 			}
 		}
 
-        return s.substr(f,max);
+        return s.substr(max_index, max_len);
     }
 };
+
+/*
+
+ 풀이법 :
+ 뒤에서부터 배열을 읽어가며, 읽는 중 앞 인덱스와, 뒤 인덱스 문자가 같은지 확인한다.
+ 일단 확인되면, 반복문을 돌려 그 사이의 문자들으 서로 같은지 확인을한다.
+ 같은 문자끼리 가장 긴 길이를 갱신하면서 계속 탐색한다.
+
 */
