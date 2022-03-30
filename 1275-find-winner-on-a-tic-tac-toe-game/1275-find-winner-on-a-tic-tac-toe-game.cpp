7ee82d5e0668cc84e0243ba6d29d8ddce7e9a558
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-30 23:31:58 +0900

    Time: 4 ms (51.63%), Space: 8.4 MB (58.33%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,7 +1,48 @@
 class Solution {
 private:
-    const size_t ARRAY_SIZE = 3; // Initialize the board, n = 3 in this problem.
+    const size_t ARRAY_SIZE = 3;
     vector<vector<int>> board;
+    
+    // Check if any of 4 winning conditions to see if the current player has won.
+    bool checkRow(int row, int player) 
+    {
+        for (size_t col = 0; col < ARRAY_SIZE; ++col) {
+            if (board[row][col] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool checkCol(int col, int player) 
+    {
+        for (size_t row = 0; row < ARRAY_SIZE; ++row) {
+            if (board[row][col] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
+        
+    bool checkDiagonal(int player) 
+    {
+        for (size_t row = 0; row < ARRAY_SIZE; ++row) {
+            if (board[row][row] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
+        
+    bool checkAntiDiagonal(int player) 
+    {
+        for (size_t row = 0; row < ARRAY_SIZE; ++row) {
+            if (board[row][ARRAY_SIZE - 1 - row] != player) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 public:
     string tictactoe(vector<vector<int>>& moves) 
@@ -9,13 +50,11 @@ public:
         board.assign(ARRAY_SIZE, vector<int>(ARRAY_SIZE, 0));
         int player = 1;
 
-        // For each move
         for (const vector<int>& move : moves) {
-            int row = move[0];
-            int col = move[1];
+            size_t row = move[0];
+            size_t col = move[1];
 
-            // Mark the current move with the current playrer's id.
-            board[row][col] = player;
+            board[row][col] = player; // Mark the current move with the current playrer's id.
 
             // If any of the winning conditions is met, return the current player's id.
             if (checkRow(row, player) || checkCol(col, player) || 
@@ -35,36 +74,4 @@ public:
         return moves.size() == 3 * 3 ? "Draw" : "Pending";
     }
     
-    // Check if any of 4 winning conditions to see if the current player has won.
-    bool checkRow(int row, int player) 
-    {
-        for (int col = 0; col < ARRAY_SIZE; ++col) {
-            if (board[row][col] != player) return false;
-        }
-        return true;
-    }
-    
-    bool checkCol(int col, int player) 
-    {
-        for (int row = 0; row < ARRAY_SIZE; ++row) {
-            if (board[row][col] != player) return false;
-        }
-        return true;
-    }
-        
-    bool checkDiagonal(int player) 
-    {
-        for (int row = 0; row < ARRAY_SIZE; ++row) {
-            if (board[row][row] != player) return false;
-        }
-        return true;
-    }
-        
-    bool checkAntiDiagonal(int player) 
-    {
-        for (int row = 0; row < ARRAY_SIZE; ++row) {
-            if (board[row][ARRAY_SIZE - 1 - row] != player) return false;
-        }
-        return true;
-    }
 };   
