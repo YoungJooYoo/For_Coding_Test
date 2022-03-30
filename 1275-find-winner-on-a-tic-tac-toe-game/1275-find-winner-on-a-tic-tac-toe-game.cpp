0561f36b36246c743ba8b171faee7adeb870f9d6
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-30 23:50:52 +0900

    Time: 4 ms (51.63%), Space: 8.4 MB (21.21%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -24,7 +24,7 @@ private:
         return true;
     }
         
-    bool checkDiagonal(int player) 
+    bool checkDiagonal(int player) // 대각선체크
     {
         for (size_t row = 0; row < ARRAY_SIZE; ++row) {
             if (board[row][row] != player) {
@@ -56,12 +56,11 @@ public:
 
             board[row][col] = player; // Mark the current move with the current playrer's id.
 
-            // If any of the winning conditions is met, return the current player's id.
             if (checkRow(row, player) || checkCol(col, player) || 
                 (row == col && checkDiagonal(player)) ||
                 (row + col == ARRAY_SIZE - 1 && checkAntiDiagonal(player))) {
                 return player == 1 ? "A" : "B";
-                return player == 1 ? "A" : "B";
+                //return player == 1 ? "A" : "B";
             }
 
             // If no one wins so far, change to the other player alternatively. 
@@ -69,9 +68,18 @@ public:
             player *= -1;
         }
 
-        // If all moves are completed and there is still no result, we shall check if 
-        // the grid is full or not. If so, the game ends with draw, otherwise pending.
         return moves.size() == 9 ? "Draw" : "Pending";
     }
-    
 };   
+
+/*
+풀이법 :
+배열의 순서가 A, B, A, B 순서로 나아간다.
+따라서 Player를 flag로 두고 1 = a, -1 = b로 둔다.
+
+주어진 함수에 따라 승자인지 판단을 한다.
+1. 현재 위치에서 row 전체가 mark 되었는지.
+2. 현재 위치에서 col 전체가 mark 되었는지
+3. 현재 위치에서 대각선(역슬래시 방향)에서 mark가 되었는지
+4. 현재 위치에서 대각선(슬래시 방향 : /)에서 mark가 되었는지
+*/
