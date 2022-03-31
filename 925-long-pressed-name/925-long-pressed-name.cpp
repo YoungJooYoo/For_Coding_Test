26803948c61dd81cafaaa83ebc1fb8cad2ad4b39
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-31 14:47:04 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (58.13%) - LeetHub

---
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   bool isLongPressedName(string name, string typed)
+   {
+       size_t i = 0;
+       size_t j = 0;
+       
+        while (j < typed.size()) {
+            if (i < name.size() && name[i] == typed[j]) { // 두 문자열을 비교, 같다면 인덱스 증가
+                ++i; 
+                ++j;
+            }        
+            else if (i > 0 && name[i - 1] == typed[j]) { // 같지 않다면,
+                ++j;
+            }
+            else {
+                return false;
+            }
+        }
+       
+        // 리턴하는 i의 인덱스가 name size와 같으면, 모든 문자열을 포함했다는 의미이므로
+        return i == name.size() ? true : false;
+    }
+};
