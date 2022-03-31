4a93169db6d353cb86142fcc6159c28fc56cfdcc
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-03-31 15:24:36 +0900

    Time: 3 ms (59.62%), Space: 6.5 MB (52.62%) - LeetHub

--- 843-guess-the-word/843-guess-the-word.cpp
+++ 843-guess-the-word/843-guess-the-word.cpp
@@ -30,11 +30,12 @@ public:
     void findSecretWord(vector<string>& wordlist, Master& master, int res = 0) 
     {
         vector<vector<int>> probs(6, vector<int>(26));
-        list<string> remWords;
-        for (const auto& w : wordlist) {
-            remWords.push_back(w);
+        list<string> remWords; // 연결리스트로 단어를 저장한다.
+        
+        for (const auto& word: wordlist) {
+            remWords.push_back(word);
             for (size_t i = 0; i < 6; ++i) {
-                probs[i][w[i] - 'a'] += 1;
+                probs[i][word[i] - 'a'] += 1;
             }
         }
         
@@ -53,3 +54,16 @@ public:
          }
     }
 };
+
+/*
+풀이법 :
+
+문제는 주어진 문자열을 저장한 배열에
+secret 문자가 무엇인지 알아 맞추는 것이다.
+
+이 문자열이 master.guess()를 호출하고
+전혀 char 전혀 매칭되는게없으면 -1
+일부 맞으면 맞은갯수 만큼 반환해서
+
+6 자리로 이루어진 scret 문자열이 무엇인지 찾는 것이다.
+*/
