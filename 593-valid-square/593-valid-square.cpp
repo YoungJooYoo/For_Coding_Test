98a2b6f34bcc821d1b2eacbac0377fa123cc355e
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-01 16:13:27 +0900

    Time: 8 ms (68.14%), Space: 26.9 MB (17.71%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -12,18 +12,18 @@ public:
         vector<vector<int>> points = {p1, p2, p3, p4}; // 주어진 배열을, 인덱스 접근이 가능한 반복문에 사용하기 위해 2차원 배열에 넣는다.
         set<double> lengths;
         
-        for (size_t i = 0; i < 4; i++) { // 각 점의 서로 모든 거리를 다 구한다.
+        for (size_t i = 0; i < 4 - 1; i++) { // 각 점의 서로 모든 거리를 다 구한다.
             for (size_t j = i + 1; j < 4; j++) {
                 double distance = get_distance(points[i], points[j]);
                 if (distance != 0) {
                     lengths.insert(distance);
                 }
-                else { // if distance is zero - we got a duplicated point
+                else { // 거리가 0이 나온다면, 두점이 겹치는 경우이므로 사각형 성립 조건이 안된다.
                     return false;
                 }
             }
         }
-        // We are supposed to end with only two different lengths: the sides and the diagonals
+        // 정사각형이라면, 변의 길이와 대간선 길이 총 2가지 종류의 길이만 나와야한다.
         return lengths.size() == 2 ? true : false;
     }
 };
