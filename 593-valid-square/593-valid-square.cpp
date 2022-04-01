6ab5c741386a993fd4167aa20d8d947dc906221c
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-01 16:10:09 +0900

    Time: 16 ms (20.66%), Space: 26.8 MB (17.71%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -12,7 +12,7 @@ public:
         vector<vector<int>> points = {p1, p2, p3, p4}; // 주어진 배열을, 인덱스 접근이 가능한 반복문에 사용하기 위해 2차원 배열에 넣는다.
         set<double> lengths;
         
-        for (size_t i = 0; i < 3; i++) { // 각 점의 서로 모든 거리를 다 구한다.
+        for (size_t i = 0; i < 4 - 1; i++) { // 각 점의 서로 모든 거리를 다 구한다.
             for (size_t j = i + 1; j < 4; j++) {
                 double curr = get_distance(points[i], points[j]);
                 if (curr != 0)
