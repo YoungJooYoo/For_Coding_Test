786e3c4ff9ab703552befb2b2413d9a5871559e5
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-01 15:39:08 +0900

    Time: 170 ms (17.91%), Space: 38.3 MB (30.75%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -3,19 +3,19 @@ public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) 
     {
         vector<vector<int>> res;
-        int  start = toBeRemoved[0];
-        int end = toBeRemoved[1];
+        int  toBeRemoved_start = toBeRemoved[0];
+        int toBeRemoved_end = toBeRemoved[1];
         
-        for (const auto& v : intervals) {
-            if (v[1] <= start || v[0] >= end) {
-                res.push_back(v);
+        for (const auto& interval : intervals) {
+            if (interval[1] <= toBeRemoved_start || interval[0] >= toBeRemoved_end) { // toBeRomoved 배열에 속하지 않는 범위면, 정답 배열에 interval을 그대로 넣는다.
+                res.push_back(interval);
             }
-            else {
-                if (v[0] < start) {
-                    res.push_back({v[0], start});
+            else { // toBeRomoved 범위안에 intervals가 있는 경우라면, 겹치지 않는 부분만 잘라서 넣는다.
+                if (interval[0] < toBeRemoved_start) {
+                    res.push_back({interval[0], toBeRemoved_start});
                 }
-                if (v[1] > end) {
-                    res.push_back({end, v[1]});
+                if (interval[1] > toBeRemoved_end) {
+                    res.push_back({toBeRemoved_end, interval[1]});
                 }
             }
         }
