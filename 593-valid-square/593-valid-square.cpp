c52fbd8b54d10278f1faa7737b983bd0bbb55cdd
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-01 16:09:43 +0900

    Time: 8 ms (68.14%), Space: 26.9 MB (17.71%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    double dist(vector<int>& p1, vector<int>& p2) // 두 점의 거리를 구하기
+    double get_distance(vector<int>& p1, vector<int>& p2) // 두 점의 거리를 구하기
     {
         const int distance_x = p2[0] - p1[0];
         const int distance_y = p2[1] - p1[1];
         return sqrt((distance_x * distance_x) + (distance_y * distance_y));
     }
     
-    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        // keep points in an array to be able to get them by index
-        vector<vector<int>> points = {p1, p2, p3, p4};
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) 
+    {
+        vector<vector<int>> points = {p1, p2, p3, p4}; // 주어진 배열을, 인덱스 접근이 가능한 반복문에 사용하기 위해 2차원 배열에 넣는다.
         set<double> lengths;
         
-        for (int i=0; i<4; i++) {
-            for (int j=i+1; j<4; j++) {
-                double curr = dist(points[i], points[j]);
+        for (size_t i = 0; i < 3; i++) { // 각 점의 서로 모든 거리를 다 구한다.
+            for (size_t j = i + 1; j < 4; j++) {
+                double curr = get_distance(points[i], points[j]);
                 if (curr != 0)
                     lengths.insert(curr);
                 
