89d2a9b98b4ba6945a4e62d7409fb74f84feee47
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-06 00:10:06 +0900

    Time: 91 ms (52.40%), Space: 23.6 MB (91.48%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -7,7 +7,7 @@ public:
         for (size_t i = 0; i < matrix.size(); i++) {
             for (size_t j = 0; j < matrix[0].size(); j++) {
                 if (i > 0 && j > 0 && matrix[i][j] == 1) {
-                    matrix[i][j] = min(matrix[i-1][j-1], min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
+                    matrix[i][j] = min(matrix[i - 1][j - 1], min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
                 }
                 result += matrix[i][j];
             }
