7ee7f0ee617f52f3881eeec0921b7323974d675b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-07 00:19:37 +0900

    Time: 505 ms (45.89%), Space: 93.2 MB (93.10%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -17,12 +17,12 @@ public:
         int y = point[1];
         
         for (const auto& elem : mp) {
-            pair<int, int>it = elem.first;
-            if (it.first != x && it.second != y && (abs(it.first - x) == abs(it.second - y))) {
-                int f1 = mp[{it.first, it.second}];
+            pair<int, int>it = elem.first; // x, y 좌표를 가져온다.
+            if (it.first != x && it.second != y && (abs(it.first - x) == abs(it.second - y))) { // abs 조건은 대각선인 경우
+                int f1 = mp[{it.first, it.second}]; // 대각선을 찾으면, 그에 기반한 온전한 사각형을 하나 만든다.
                 int f2 = mp[{it.first, y}];
                 int f3 = mp[{x, it.second}];
-                count += f1 * f2 * f3;
+                count += f1 * f2 * f3; // 각 점의 나온 횟수를 곱하면 그게 카운트가 된다. 그리고 카운트를 누적한다.
             }
         }
         
