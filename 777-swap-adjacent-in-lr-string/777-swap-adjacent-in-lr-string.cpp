1a19529db6f3fc187b5a4ca7e318ff7695e016f8
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-07 15:24:04 +0900

    Time: 8 ms (69.65%), Space: 7.5 MB (41.99%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
@@ -46,3 +46,14 @@ public:
         return true;
     }
 };
+
+/*
+n the given string start, 
+L can move to the left but can't pass through R, 
+R can move to the right but can't pass through L. 
+Basically, there are 4 situations (when start[i] != 'X' and end[j] != 'X'):
+
+start[i] != end[j] return false, cause L and R can't pass through each other.
+start[i] == 'L' && j > i return false, cause L can't move to the right.
+start[i] == 'R' && j < i return false cause R can't move to the left.
+*/
