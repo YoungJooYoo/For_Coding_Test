8d9bb1c64e4492d7a51e6929976c24e0f08b687e
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-07 16:55:00 +0900

    Time: 11 ms (36.67%), Space: 8 MB (55.07%) - LeetHub

---
+++ 809-expressive-words/809-expressive-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+       int expressiveWords(string s, vector<string>& words) 
+       {
+            int res = 0;
+           
+            for (const string& word : words) {
+                if (check(s, word)) {
+                    res++; 
+                }
+            }
+   
+         return res;
+    }
+
+    bool check(string s, string w) 
+    {
+        const size_t n = s.size();
+        const size_t m = w.size();
+        size_t j = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (j < m && s[i] == w[j]) {
+                j++;
+            }
+            else if (i > 1 && s[i - 2] == s[i - 1] && s[i - 1] == s[i]);
+            else if (0 < i && i < n - 1 && s[i - 1] == s[i] && s[i] == s[i + 1]);
+            else {
+                return false;
+            }
+        }
+        
+        return j == m  ? true : false;
+    }
+};
