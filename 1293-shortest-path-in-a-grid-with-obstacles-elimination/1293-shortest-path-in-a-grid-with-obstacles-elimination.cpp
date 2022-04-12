2ab3ad43c489e64b1ad800dcb64a0f2abab1dec7
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-13 00:36:20 +0900

    Time: 272 ms (30.89%), Space: 50.7 MB (32.32%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -7,35 +7,35 @@ public:
         vector<vector<int>> visited(m, vector<int>(n, -1)); // This vector stores the number of obstacles that we can still remove after walking through that cell
         queue<vector<int>> q;
         
-        q.push({0, 0, 0, k}); // // x, y, currentLength, remaining k
+        q.push({0, 0, 0, k}); // x, y, currentLength, remaining k
         
         while (!q.empty())
         {    
             vector<int> t = q.front();
             q.pop();
             
-            int x = t[0], y = t[1];
+            int x = t[0];
+            int y = t[1];
             
             // Invalid cases being dealt here since it's easier to write one condition instead of 4 while pushing.
             if (x < 0 || x >= m || y < 0 || y >= n) {
                 continue;
             }
-            // If you've reached the end, great, return the currentLength!
-            if(x == m-1 && y == n-1)
+            if (x == m - 1 && y == n - 1) {// If you've reached the end, great, return the currentLength!
                 return t[2]; //currentLength of the path
-             
-            // If we hit an obstacle & we don't have any Ks remaining, continue
-            // If we still have Ks to spend, we spend 1.
-            if(grid[x][y] == 1){
-                if(t[3] > 0)
-                    t[3]--;
-                else
+            }
+            if (grid[x][y] == 1) { // If we hit an obstacle & we don't have any Ks remaining, continue
+                if (t[3] > 0) {
+                    t[3]--; // If we still have Ks to spend, we spend 1.
+                }
+                else {
                     continue;
+                }
             }
             
             // If this cell is already visited with a K value lesser than this one, we would want to save Ks for future use, so we continue
             // This is the most important condition and part of the solution!
-            if(visited[x][y]!=-1 && visited[x][y] >= t[3]){
+            if (visited[x][y] != -1 && visited[x][y] >= t[3]) {
                 continue;
             }
             
@@ -43,10 +43,10 @@ public:
             visited[x][y] = t[3];
                 
             // Push the adjacent nodes in the queue.
-            q.push({x+1, y, t[2]+1, t[3]});
-            q.push({x-1, y, t[2]+1, t[3]});
-            q.push({x, y+1, t[2]+1, t[3]});
-            q.push({x, y-1, t[2]+1, t[3]});
+            q.push({x + 1, y, t[2] + 1, t[3]});
+            q.push({x - 1, y, t[2] + 1, t[3]});
+            q.push({x, y + 1, t[2] + 1, t[3]});
+            q.push({x, y - 1, t[2] + 1, t[3]});
         }
         
         return -1;
