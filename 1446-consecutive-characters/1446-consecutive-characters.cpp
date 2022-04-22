948bfba7b38e84f4be6c10bce2de352ae3e13e2b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-22 22:23:28 +0900

    Time: 10 ms (12.25%), Space: 6.9 MB (27.64%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxPower(string s) 
+    {
+        int max_length = 0;
+        int count = 1;
+        
+        if (s.size() == 1) {
+            return 1;
+        }
+        
+        for (size_t i = 1; i < s.size(); i++) {
+            if (s[i - 1] == s[i]) {
+                count++;
+            }
+            else {
+                //max_length = max(max_length, count);
+                count = 1;
+            }
+            max_length = max(max_length, count);
+        }
+        
+        return max_length;
+    }
+};
