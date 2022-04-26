a288172654bc8de35c462c88a53fa71afa2009c2
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-26 22:14:24 +0900

    Time: 3 ms (31.45%), Space: 6.1 MB (22.62%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     string defangIPaddr(string address) 
     {
-        // 원소단위로 복사
-        // . 나온순간 => [.] 붙이기
         for (size_t i = 0; i < address.size(); i++) {
             if (address[i] == '.') {
                 ans = ans + "[.]";
