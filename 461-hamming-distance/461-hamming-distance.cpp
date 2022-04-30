b25eceaf4ade94b1ce8784476441801cc9ccef43
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-04-30 18:42:56 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (28.01%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -5,7 +5,7 @@ public:
         int res = 0;
         int num = x^y; // 비트가 다른 부분만 1로 남긴다.
         
-        while (num) { // 남겨진 1의 갯수를 카운트한다.
+        while (num != 0) { // 남겨진 1의 갯수를 카운트한다.
             res = res + (num % 2);
             num = num >> 1; // 우측으로 비트 하나씩 밀어낸다. 나누기 2 효과
         }
