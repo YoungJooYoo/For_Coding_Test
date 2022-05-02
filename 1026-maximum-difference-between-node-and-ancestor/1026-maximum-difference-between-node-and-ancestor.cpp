2087340a74db37e44adfc0384d16d2a00daaff69
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-03 00:01:33 +0900

    Time: 4 ms (80.94%), Space: 9.8 MB (80.33%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -12,3 +12,11 @@ public:
         return max(maxAncestorDiff(root->left, min_val, max_val), maxAncestorDiff(root->right, min_val, max_val));
     }
 };
+
+/*
+풀이법 :
+재귀로 탐색하면서, 좌우로 노드가 갈라지며 탐색할때마다
+노드의 값을 체크해 최대값 최소값을 업데이트 한다.
+트리의 마지막 끝 부분에 도달하면 최종적으로 max_val - min_val을 반환하고, 이 반환된 값에서 다시 max값을 뽑으면서
+재귀를 반복한다.
+*/
