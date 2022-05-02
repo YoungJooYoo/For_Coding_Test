50fb38c4585a3f9632891424464b2a5289164109
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-02 22:15:02 +0900

    Time: 4 ms (86.01%), Space: 7.9 MB (74.44%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
     {
         vector<int> result(n + 1, 0); // result[0]은 0으로 자동 초기화
         for (size_t i = 1; i <= n; i++) { // 1부터 시작해서 구한다.
-            result[i] = result[i >> 1] + (i & 1); // i >> i 배열의 인덱스의 비트 연산을 통해 인덱스 절반을 구한다.
+            result[i] = result[i >> 1] + (i & 1); // i >> i 배열의 인덱스의 비트 연산을 통해 인덱스 절반을 구한다. i & 1 => 짝수면 0 홀수면 1이 나오게 한다.
         }
         return result;
     }
