21e3ccaf9a44e4de69ae5be79f1f6d4f6f0269eb
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-03 23:02:01 +0900

    Time: 82 ms (53.27%), Space: 33.7 MB (50.92%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -3,15 +3,12 @@ public:
     vector<int> findDuplicates(vector<int>& nums) 
     {
         vector<int> ans;
-        unordered_map<int, int> mp;
         
-        for (size_t i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++;
-        }
+        sort(nums.begin(), nums.end());
         
-        for (const auto& ele : mp) {
-            if (ele.second > 1) {
-                ans.push_back(ele.first);
+        for (size_t i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                ans.push_back(nums[i]);
             }
         }
         
