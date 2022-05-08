5f6f8b0357da6d817bba5462c2d0865696d58bd9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-08 15:55:51 +0900

    Time: 26 ms (29.33%), Space: 22.8 MB (12.99%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) 
     {
-        //if (!root) return {};
         queue<TreeNode*> q;
-        q.push(root);
+        TreeNode* curr;
         vector<double> res;
-        vector<int> tmp;
-        int len, i;
-        TreeNode *curr;
+        //vector<int> tmp;
+        size_t len;
+        
+        q.push(root);
+        
         while (q.size()) {
-            tmp.clear();
-            len = q.size();
-            for (i = 0; i < len; i++) {
+            //tmp.clear(); // bfs 탐색 중, 새로운 레벨로 되면 큐를 밀어서 다시 값을 채운다.
+            vector<int> tmp;
+            size_t len = q.size(); // bfs, 탐색 중, 탐색 레벨이 내려갈수록 노드의 갯수가 다르니, 사이즈를 다시 계산한다.
+            for (size_t i = 0; i < len; i++) {
                 curr = q.front();
                 tmp.push_back(curr->val);
                 if (curr->left) q.push(curr->left);
@@ -21,6 +23,7 @@ public:
             }
             res.push_back(accumulate(begin(tmp), end(tmp), 0.0) / tmp.size());
         }
+        
         return res;
     }
 };
