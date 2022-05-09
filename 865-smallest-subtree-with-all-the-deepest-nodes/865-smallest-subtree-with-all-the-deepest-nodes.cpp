e4728cb01394a98638671c026d3a979a71ca7269
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-10 00:45:07 +0900

    Time: 6 ms (63.18%), Space: 14.5 MB (46.37%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int getDepth(TreeNode* root) {
-        if (!root)
+    int getDepth(TreeNode* root) 
+    {
+        if (root == nullptr) {
             return 0;
+        }
         return max(getDepth(root->right), getDepth(root->left)) + 1;
+        
     }
     
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        if (!root)
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) 
+    {
+        if (root == nullptr) {
             return NULL;
+        }
         
         int right_depth = getDepth(root->right);
         int left_depth = getDepth(root->left);
         
-        if (right_depth == left_depth)
+        if (right_depth == left_depth) {
             return root;
-        
-        if (right_depth > left_depth) 
+        }
+        else if (right_depth > left_depth) {
             return subtreeWithAllDeepest(root->right);
-        
-        else 
+        }
+        else {
             return subtreeWithAllDeepest(root->left);
+        }
     }
 };
