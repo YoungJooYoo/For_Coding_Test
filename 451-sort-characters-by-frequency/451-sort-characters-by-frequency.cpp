6f2c32a15ae3b69740706e22b66c2a67b422b2f3
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-09 21:35:59 +0900

    Time: 14 ms (62.82%), Space: 8.6 MB (26.53%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string frequencySort(string s) 
     {
-        unordered_map<char, int> freq;
-        priority_queue<pair<int, char>> q;
-        pair<int, char> curr;
+        unordered_map<char, int> freq; // 알파벳별 빈도수
+        priority_queue<pair<int, char>> q; //  빈도수 - 알파벳 묶음
+        pair<int, char> curr; // 우선순위 큐에 넣은 pair
         string res;
         
         // 단어의 빈도수 체크
@@ -15,7 +15,6 @@ public:
         for (const auto& [a, frq] : freq) {
             q.push({frq, a});
         }
-        
         
         while (!q.empty()) {
             curr = q.top();
