8072deb7ee825dbbf866923d296714c058c7f816
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-10 21:16:27 +0900

    Time: 36 ms (99.73%), Space: 15.3 MB (89.75%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) 
     {
-        int cnt[26] = {0, }; 
+        int count_chars[26] = {0, }; 
         int res = 0;
         
         // 알파벳의 빈도수 체크
         for (const char& ch : chars) {
-            ++cnt[ch - 'a'];
+            ++count_chars[ch - 'a'];
         }
         
         for (const string& w : words) {
             int cnt1[26] = {};
             bool match = true;
             for (auto ch : w) {
-                if (++cnt1[ch - 'a'] > cnt[ch - 'a']) {
+                if (++cnt1[ch - 'a'] > count_chars[ch - 'a']) {
                     match = false;
                     break;
                 }
