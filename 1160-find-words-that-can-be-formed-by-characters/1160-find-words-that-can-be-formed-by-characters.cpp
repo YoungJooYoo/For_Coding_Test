b3b8f715ec43c3816bd50fa9b05375c84a3b277a
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-10 21:20:36 +0900

    Time: 76 ms (64.26%), Space: 15.3 MB (96.70%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -13,8 +13,8 @@ public:
         for (const string& word : words) {
             int word_count[26] = {0, };
             bool match = true;
-            for (const char& ch : word) {
-                if (++word_count[ch - 'a'] > count_chars[ch - 'a']) {
+            for (const char& ch : word) { // 배열에 있는 문자열의 빈도수를 체크한다.
+                if (++word_count[ch - 'a'] > count_chars[ch - 'a']) { // 빈도수가 일치 하지 않으면 false처리,  chars가 words를 내포해야하는 조건식
                     match = false;
                     break;
                 }
