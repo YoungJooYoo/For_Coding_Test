02b968489587191ceab4e97f484e709bbafbb78f
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-10 17:05:32 +0900

    Time: 9 ms (17.28%), Space: 7 MB (54.02%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -3,7 +3,7 @@ public:
     int myAtoi(string s) 
     {
         int res = 0;
-        int sign = 1;
+        int sign = 1; // 1 양수, -1 음수
 		size_t i = 0;
         
         // 공백문자 건너 띄기
@@ -12,19 +12,17 @@ public:
         }  
         
         // 음수 체크
-        if(s[i]=='-' || s[i]=='+') {      
+        if (s[i]=='-' || s[i]=='+') {      
             sign = s[i] =='-'?-1:1;
             i++;
         }
      
-        
-        // now iterate across digits if any
-		// should only be in range 0-9
+		// 숫자 범위의 0~9 까지 문자를 검출해 int로 변환한다.
         while (i < s.length() && (s[i] >= '0' && s[i] <= '9')) { //traverse string till nondigit not found or string ends
             int digit = (s[i] - '0') * sign;
             if (sign == 1 && (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > INT_MAX % 10))) {
-                return INT_MAX; 
-            } //check for overflow
+                return INT_MAX;  //check for overflow
+            } 
             if (sign == -1 && (res < INT_MIN / 10 || (res == INT_MIN / 10 && digit < INT_MIN % 10))) {
                 return INT_MIN; //check for underflow
             }
