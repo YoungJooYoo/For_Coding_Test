0b8369a95f60583dbbba73d341107116a7869794
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-11 10:55:51 +0900

    Time: 0 ms (100.00%), Space: 7 MB (95.54%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+        vector<int> curr;
+        int size;
+        
+        for (auto num : nums) {
+            size = res.size();
+            
+            for (int i = 0; i < size; i++) {
+                curr = res[i];
+                curr.push_back(num);
+                res.push_back(curr);
+            }
+        }
+        
+        return res;
+    }
+};
