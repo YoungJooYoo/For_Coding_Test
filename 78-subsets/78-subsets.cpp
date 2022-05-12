870fb251a935fd7951561005025223ce245f15be
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-13 00:05:24 +0900

    Time: 5 ms (34.04%), Space: 7 MB (86.09%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -7,9 +7,10 @@ public:
         
         for (const int& num : nums) {
             size_t array_size = res.size();
+            cout << array_size << endl;
             
             for (size_t i = 0; i < array_size; i++) {
-                curr = res[i];
+                curr = res[i]; // 2차원 배열의 행(일차원배열)을 curr에 대입한다.
                 curr.push_back(num);
                 res.push_back(curr);
             }
