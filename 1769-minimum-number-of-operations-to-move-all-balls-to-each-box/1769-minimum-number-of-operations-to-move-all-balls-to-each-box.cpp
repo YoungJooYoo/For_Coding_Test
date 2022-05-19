b2a0213b5e3361a99883ed065a5fe510428678fb
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-19 23:51:41 +0900

    Time: 7 ms (86.77%), Space: 8.7 MB (97.60%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) 
-    {
+	{
+	    int sum = 0;
+		int ballSeen = 0;
         const size_t BOXES_LENGTH = boxes.size();
-        vector<int> res(BOXES_LENGTH);
+		vector<int> result(BOXES_LENGTH, 0);
+                
+        for (size_t i = 1; i < BOXES_LENGTH; ++i) {
+		    if( boxes[i-1] =='1') { 
+			    ballSeen += 1;
+			}
+			sum += ballSeen;
+			result[i] += sum;
+		 }
+
+		 sum = 0;
+		 ballSeen = 0;
         
-        for (size_t i = 0, ops = 0, cnt = 0; i < BOXES_LENGTH; ++i) {
-           res[i] += ops;
-           cnt += boxes[i] == '1' ? 1 : 0;
-           ops += cnt;
-        }
-        for (int i = BOXES_LENGTH - 1, ops = 0, cnt = 0; i >= 0; --i) {
-            res[i] += ops;
-            cnt += boxes[i] == '1' ? 1 : 0;
-            ops += cnt;
-        }
-        
-        return res;
+		 for (int i = BOXES_LENGTH - 2; i >= 0; --i) {
+			 if (boxes[i + 1] =='1') { 
+				 ballSeen += 1;
+			 }
+			 sum += ballSeen;
+			 result[i] += sum;
+		 }
+		 
+		return result;
     }
 };
