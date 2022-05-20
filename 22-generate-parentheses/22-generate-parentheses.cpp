db893c79391168f149bfd981258ef0dc65c1558a
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-21 00:03:31 +0900

    Time: 12 ms (20.38%), Space: 13.8 MB (46.60%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -8,10 +8,10 @@ public:
         }
         
         if (open > 0) {
-            recursive(curr + "(", open - 1, close + 1);
+            recursive(curr + "(", open - 1, close + 1); // opne 괄호를 추가하면  opne - 1하고, 닫아야하는 괄호 + 1
         } 
         if (close > 0) {
-            recursive(curr + ")", open, close - 1);
+            recursive(curr + ")", open, close - 1); // close 괄호를 추가하면  opne + 1하고, 닫아야하는 괄호 - 1
         }
     }
     
@@ -24,3 +24,11 @@ public:
 private:
     vector<string> res;
 };
+
+/*
+풀이법 :
+재귀에 가장 좋은 시각화 자료 :
+https://youtu.be/s9fokUqJ76A?t=343
+
+즉 시작은 ( 이 괄호 부터 가능하며, 이 괄화 갯수에 따라 호출되는 재귀 함수를 통해
+*/
