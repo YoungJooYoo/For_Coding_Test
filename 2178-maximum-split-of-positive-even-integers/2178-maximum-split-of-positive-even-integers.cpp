8012c0189cb8f74a8bc05e9dec54fe4df1e90e06
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-24 23:41:36 +0900

    Time: 235 ms (34.23%), Space: 40.5 MB (50.14%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -4,21 +4,20 @@ public:
     {
         vector<long long> ans;
         long long i = 2;
-        long long crSum = 0;
+        long long sum = 0;
         
         if (FINAL_SUM % 2 != 0) {
             return ans;
         }
         
-        while (crSum + i <= FINAL_SUM) {
+        while (sum + i <= FINAL_SUM) {
             ans.push_back(i);
-            crSum += i;
-            i += 2;
+            sum += i; // 누적값을 체크한다.
+            i += 2; // 배열의 원소는 짝수므로 +2 씩
         }
 		
-		
-		const size_t ARRAY_SIZE = ans.size(); // add remaining difference to the last value in answer list
-        ans[ARRAY_SIZE - 1] += (FINAL_SUM - crSum);
+		const size_t ARRAY_SIZE = ans.size(); // 배열의 마지막 원소를 구하기
+        ans[ARRAY_SIZE - 1] += (FINAL_SUM - sum); 
         
         return ans;
     }
