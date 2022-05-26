cfec7b9bcc2f306ca66cd1e35bb79ddc679f03b9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-26 23:52:55 +0900

    Time: 46 ms (44.91%), Space: 18.7 MB (46.57%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -3,8 +3,6 @@ public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) 
     {
         vector<vector<int>> ans;
-        size_t i = 0;
-        size_t j = 0;
         
         for (size_t i = 0, j = 0; i < firstList.size() && j < secondList.size(); firstList[i][1] < secondList[j][1] ? ++i : ++j) 
         {
