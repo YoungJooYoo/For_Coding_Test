c9089b95b2ce8b95d0ee106c4ec06e32d262ca82
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-28 17:28:22 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (92.15%) - LeetHub

---
+++ 818-race-car/818-race-car.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int racecar(int target) 
+    {
+        if (dp[target] > 0) {
+            return dp[target];
+        }
+        
+        int n = floor(log2(target)) + 1;
+        int res;
+        
+        if (1 << n == target + 1) {
+            dp[target] = n;
+        }
+        else {
+            dp[target] = racecar((1 << n) - 1 - target) + n + 1;
+            for (int m = 0; m < n - 1; ++m)
+                dp[target] = min(dp[target], racecar(target - (1 << (n - 1)) + (1 << m)) + n + m + 1);
+        }
+        
+        return dp[target];
+    }
+private:    
+    int dp[10001];
+};
