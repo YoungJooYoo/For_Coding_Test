a49a48c79d3467ac933b88480153c271f7826710
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-05-28 18:41:12 +0900

    Time: 3 ms (91.40%), Space: 6.1 MB (96.53%) - LeetHub

--- 818-race-car/818-race-car.cpp
+++ 818-race-car/818-race-car.cpp
@@ -14,8 +14,9 @@ public:
         }
         else {
             dp[target] = racecar((1 << n) - 1 - target) + n + 1;
-            for (int m = 0; m < n - 1; ++m)
+            for (int m = 0; m < n - 1; ++m) {
                 dp[target] = min(dp[target], racecar(target - (1 << (n - 1)) + (1 << m)) + n + m + 1);
+            }
         }
         
         return dp[target];
