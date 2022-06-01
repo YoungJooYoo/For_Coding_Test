2d5449df89e5676da0b8159698c87436b13092c9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-02 01:41:29 +0900

    Time: 189 ms (15.54%), Space: 67.2 MB (23.31%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -3,21 +3,10 @@ public:
     int numOfPairs(vector<string>& nums, string target) 
     {
         int ans = 0;
-        const size_t NUMS_LENGTH = nums.size();
         
-        for (size_t i = 0; i < NUMS_LENGTH - 1; i++) {
-            for (size_t j = i + 1; j < NUMS_LENGTH; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans++;
-                }
-            }
-        }
-        
-        reverse(nums.begin(), nums.end());
-        
-        for (size_t i = 0; i < NUMS_LENGTH - 1; i++) {
-            for (size_t j = i + 1; j < NUMS_LENGTH; j++) {
-                if (nums[i] + nums[j] == target) {
+        for (size_t i = 0; i < nums.size(); i++) {
+            for (size_t j = 0; j < nums.size(); j++) {
+                if (i != j and target == nums[i] + nums[j]) {
                     ans++;
                 }
             }
