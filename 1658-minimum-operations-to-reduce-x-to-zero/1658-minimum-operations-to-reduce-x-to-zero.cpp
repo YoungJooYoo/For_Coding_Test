e77d2e4bcf78cb0e06572999c10411a864986bd3
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-11 15:11:39 +0900

    Time: 202 ms (80.55%), Space: 98.5 MB (71.00%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) 
+    {
+        const int ARR_SIZE = nums.size();
+        int total = 0;
+        int current = 0;
+        int ans = -1;
+        int left = 0;
+        
+        // 전체 합 구하기
+        for (const int& num : nums) {
+            total += num;
+        }
+
+        for (int right = 0; right < ARR_SIZE; right++) {
+            // sum([left ,..., right]) = total - x
+            current += nums[right];
+            // if larger, move `left` to left
+            while (current > total - x && left <= right) {
+                current -= nums[left];
+                left += 1;
+            }
+            if (current == total - x) {
+                int length = right - left + 1;
+                ans = max(ans, length);
+            }
+        }
+        
+        if (ans != -1) {
+            return ARR_SIZE - ans;
+        }
+        
+        return -1;
+    }
+};
