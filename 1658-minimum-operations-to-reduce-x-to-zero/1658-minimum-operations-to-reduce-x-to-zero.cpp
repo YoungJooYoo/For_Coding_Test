fbd0736ad20f5fa8d5ace62aff1a0ed2a31d4fb8
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-11 16:00:02 +0900

    Time: 195 ms (84.02%), Space: 98.5 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) 
+    int minOperations(vector<int>& n, int x) 
     {
-        const int ARR_SIZE = nums.size();
-        int sum = accumulate(begin(nums), end(nums), 0);
-        int left = 0;
-        int right = 0;
-        int res = INT_MAX;
+          int size = n.size();
+          int total = 0;
+          int len = 0;
+          for (int i : n) total += i;
+          if (total == x) return size;
         
-        while (left <= right) {
-            if (sum >= x) {
-                if (sum == x) {
-                    res = min(res, left + ARR_SIZE - right);
-                }
-                if (right < ARR_SIZE) {
-                    sum -= nums[right];
-                    right++;
-                }
-                else {
-                    break;
-                }
-            }
-            else { // sum < x
-                sum += nums[left];
-                left++;
-            }
-        }
+          int need = total - x;
+          int i = 0 , j = 0;
+          int sum = 0;
         
-        if (res == INT_MAX) {
-            return -1;
-        }
-        
-        return res;
+          while( j < size ) {
+                sum += n[j];  
+                while(i < j && sum > need){
+                        sum -= n[i++];
+                }
+                if(sum == need){
+                    
+                    len = max(len, j-i+1);
+                }
+                j++;
+          } 
+          if(len == 0) return -1;  
+          return size-len;
     }
 };
