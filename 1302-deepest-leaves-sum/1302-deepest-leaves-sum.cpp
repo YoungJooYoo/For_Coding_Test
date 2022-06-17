94e94fcfe7912126293ae974886aae49ae1d6ca8
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-18 01:51:53 +0900

    Time: 104 ms (95.43%), Space: 59.9 MB (56.81%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,47 @@
+class Solution 
+{
+public:
+    void get_depth(TreeNode* root, int depth) 
+    {
+        if (root == nullptr) {
+            return;
+        }
+        
+        depth++;
+        max_depth = max(max_depth, depth);
+        get_depth(root->left, depth);
+        get_depth(root->right, depth);
+    }
+    
+    void get_deepest_sum(TreeNode* root, int depth)
+    {
+        if (root == nullptr) {
+            return;
+        }
+        
+        depth++;
+        
+        if (depth == max_depth) {
+            sum += root->val;
+        } 
+        
+        get_deepest_sum(root->left, depth);
+        get_deepest_sum(root->right, depth);
+
+        
+    }
+    
+    int deepestLeavesSum(TreeNode* root) 
+    {
+        get_depth(root, 0);
+        get_deepest_sum(root, 0);
+        
+        cout << max_depth << endl;
+        
+        return sum;
+    }
+
+private:
+    int max_depth = 0;
+    int sum = 0;
+};
