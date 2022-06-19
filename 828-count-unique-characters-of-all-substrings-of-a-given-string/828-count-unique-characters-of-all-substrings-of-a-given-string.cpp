d329c7fef117132537f0fdded8c716f5e1a4f431
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-19 12:51:45 +0900

    Time: 124 ms (27.48%), Space: 10 MB (99.20%) - LeetHub

---
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+    int uniqueLetterString(string& s) 
+    {
+        int ans = 0;
+        
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            int cnt = 0;
+            int curr = -1;
+            int prev = -1;
+            
+            for (int i = 0; i < s.size(); i++) {
+                if (s[i] == ch) {
+                    cnt++; // 알파벳의 빈도수 증가
+                    prev = curr; // 이전 인덱스 기억
+                    curr = i; // 현재 인덱스 갱신
+                }
+                if (cnt == 1) { // 유니크한 경우
+                    ans += curr + 1;
+                } 
+                else {
+                    ans += curr - prev;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
