f272593d670781c464c42a74dae54a7095fc61a5
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-19 15:16:13 +0900

    Time: 70 ms (59.12%), Space: 11.4 MB (85.52%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,21 +1,22 @@
 
 class Solution {
  public:
-  int uniqueLetterString(string s) {
-    int ans = 0;
-    int count = 0;
-    vector<int> lastCount(26);
-    vector<int> lastSeen(26, -1);
+    int uniqueLetterString(string s) 
+    {
+        int ans = 0;
+        int count = 0;
+        vector<int> lastCount(26, 0);
+        vector<int> lastSeen(26, -1);
 
-    for (int i = 0; i < s.length(); ++i) {
-      const int c = s[i] - 'A';
-      const int currentCount = i - lastSeen[c];
-      count = count - lastCount[c] + currentCount;
-      lastCount[c] = currentCount;
-      lastSeen[c] = i;
-      ans += count;
-    }
+        for (size_t i = 0; i < s.length(); ++i) {
+            const int c = s[i] - 'A'; // 0 ~ 26까지 숫자가 나온다. 알파벳 A ~ Z
+            const int currentCount = i - lastSeen[c];
+            count = count - lastCount[c] + currentCount;
+            lastCount[c] = currentCount;
+            lastSeen[c] = i;
+            ans += count;
+        }
 
-    return ans;
-  }
+        return ans;
+     }
 };
