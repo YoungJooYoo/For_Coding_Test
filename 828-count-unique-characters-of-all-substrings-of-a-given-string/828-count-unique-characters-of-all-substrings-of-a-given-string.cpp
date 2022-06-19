5982aa68791c4de43991573636395c23040cd3ae
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-19 15:11:44 +0900

    Time: 87 ms (45.61%), Space: 11.5 MB (63.85%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,30 +1,21 @@
-class Solution 
-{
-public:
-    int uniqueLetterString(string& s) 
-    {
-        int ans = 0;
-        
-        for (char ch = 'A'; ch <= 'Z'; ch++) { // 알파벳 순서대로 검출하기
-            int cnt = 0;
-            int curr = 0; // init  부분 문자열 길이의 전체 합
-            int prev = 0; // init  이전 부분 문자열  전체 합
-            
-            for (int i = 0; i < s.size(); i++) {
-                if (s[i] == ch) {
-                    cnt++; // 알파벳의 빈도수 증가
-                    prev = curr; // prev 갱신
-                    curr = i; // 갱신
-                }
-                if (cnt == 1) { // 유니크한 경우
-                    ans += curr + 1; // 인덱스 + 1 = 길이
-                } 
-                else { // cnt != 1 아닌 경우 빼기. 유니크하지 않아, 이전에 더한 것 빼버리기
-                    ans += curr - prev;
-                }
-            }
-        }
-        
-        return ans;
+
+class Solution {
+ public:
+  int uniqueLetterString(string s) {
+    int ans = 0;
+    int count = 0;
+    vector<int> lastCount(26);
+    vector<int> lastSeen(26, -1);
+
+    for (int i = 0; i < s.length(); ++i) {
+      const int c = s[i] - 'A';
+      const int currentCount = i - lastSeen[c];
+      count = count - lastCount[c] + currentCount;
+      lastCount[c] = currentCount;
+      lastSeen[c] = i;
+      ans += count;
     }
+
+    return ans;
+  }
 };
