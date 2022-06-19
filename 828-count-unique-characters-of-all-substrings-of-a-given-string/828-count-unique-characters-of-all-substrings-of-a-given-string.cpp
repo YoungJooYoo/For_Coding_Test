d31c3d9c1c0042c55436a789bef177c5585bafb1
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-19 14:17:26 +0900

    Time: 177 ms (17.39%), Space: 10.2 MB (98.75%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -5,21 +5,21 @@ public:
     {
         int ans = 0;
         
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
+        for (char ch = 'A'; ch <= 'Z'; ch++) { // 알파벳 순서대로 검출하기
             int cnt = 0;
-            int curr = 0;
-            int prev = 0;
+            int curr = 0; // init  부분 문자열 길이의 전체 합
+            int prev = 0; // init  이전 부분 문자열  전체 합
             
             for (int i = 0; i < s.size(); i++) {
                 if (s[i] == ch) {
                     cnt++; // 알파벳의 빈도수 증가
-                    prev = curr; // 이전 인덱스 기억
-                    curr = i; // 현재 인덱스 갱신
+                    prev = curr; // prev 갱신
+                    curr = i; // 갱신
                 }
                 if (cnt == 1) { // 유니크한 경우
-                    ans += curr + 1;
+                    ans += curr + 1; // 인덱스 + 1 = 길이
                 } 
-                else {
+                else { // cnt != 1 아닌 경우 빼기. 유니크하지 않아, 이전에 더한 것 빼버리기
                     ans += curr - prev;
                 }
             }
