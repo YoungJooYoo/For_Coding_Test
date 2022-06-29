8c164085d9032fe40b6a600db70e63e6e49ff4e1
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-06-30 00:39:13 +0900

    Time: 50 ms (75.50%), Space: 28.1 MB (66.58%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -5,15 +5,19 @@ public:
         int seen[60] = {0, };
         int count = 0;
         
-        // 숫자가 어떻든 나머지 패턴은 같으므로
+        // 숫자가 어떻든 나머지 패턴은 같으므로, 배열의 원소들의 나머지만 구한다.
         for (int& ele : time) {
             ele = ele % 60;
         }
         
-        for (const int& ele : time) {
+        for (const int& ele : time) { // 60 = ele  + x   ->  60 - ele = x    => 즉 ele를 알고 있으므로 짝이되는 x를 구하는 것
             int x = 60 - ele;
             count += seen[x % 60]; // 60 - (ele % 60) 나머지 구하고  -> 60 - 나머지  =  
             seen[ele % 60]++;
+        }
+        
+        for (auto ele : seen) {
+            cout << ele << " ";
         }
         
         return count;
