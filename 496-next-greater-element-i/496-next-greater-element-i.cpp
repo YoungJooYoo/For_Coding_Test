db4e1c3996523457c1cf8c6b9223e7ccb42a3959
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-07-08 19:49:24 +0900

    Time: 14 ms (34.94%), Space: 8.8 MB (69.75%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) 
+    {
+        stack<int> s;
+        unordered_map<int, int> m;
+        vector<int> ans;
+        
+        for (const int& num : nums) {
+            while (s.size() != 0 && s.top() < num) {
+                m[s.top()] = num;
+                s.pop();
+            }
+            s.push(num);
+        }
+        
+        for (const int& n : findNums) {
+            ans.push_back(m.count(n) ? m[n] : -1);
+        }
+        
+        return ans;
+    }
+};
