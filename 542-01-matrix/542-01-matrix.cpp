47786fc3e50319d59be9d432309bb40d897ced57
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-07-24 18:34:46 +0900

    Time: 191 ms (16.48%), Space: 30.2 MB (51.34%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,29 +1,41 @@
-class Solution {
+class Solution 
+{
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
-        //Save all cells of 0 into a queue. Change cells of 1 into INT_MAX.
-        queue<pair<int, int>> q;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (mat[i][j] == 0) q.push({i, j});
-                else mat[i][j] = INT_MAX;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
+    {
+        const int ROW = mat.size();
+        const int COL = mat[0].size();
+        vector<vector<int>> directions{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        queue<pair<int, int>> job_queue;
+        
+        for (int i = 0; i < ROW; ++i) {
+            for (int j = 0; j < COL; ++j) {
+                if (mat[i][j] == 0) {
+                    job_queue.push({i, j});
+                }
+                else {
+                    mat[i][j] = INT_MAX;
+                }
             }
         }
-        while (!q.empty()) {
-            // Get a 0 cell t from queue. 
-            pair<int, int> t = q.front(); q.pop();
-            // Move to a one step away from this cell. 
-            for (auto dir : dirs) {
-                int x = t.first + dir[0], y = t.second + dir[1];
+        
+        while (!job_queue.empty()) {
+            auto curr_location = job_queue.front(); job_queue.pop();
+            
+            for (const auto& direction : directions) {
+                int m = curr_location.first;
+                int n = curr_location.second;
+                int x = m + direction[0];
+                int y = n + direction[1];
                 // If it steps out of the matrix, or the adjacent cell already records a smaller distance (for instance, the adjacent cell is also 0). 
-                if (x < 0 || x >= m || y < 0 || y >= n || mat[x][y] <= mat[t.first][t.second] + 1) continue;
-                // Adds one distance to the adjacent cell.
-                mat[x][y] = mat[t.first][t.second] + 1;
-                q.push({x, y});
+                if (x < 0 || x >= ROW || y < 0 || y >= COL || mat[x][y] <= mat[m][n] + 1) {
+                    continue;
+                }
+                mat[x][y] = mat[m][n] + 1; // Adds one distance to the adjacent cell.
+                job_queue.push({x, y});
             }
         }
+        
         return mat;
     }
 };
