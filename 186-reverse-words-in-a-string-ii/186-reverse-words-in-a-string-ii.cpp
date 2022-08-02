4755add5bcda953b24a60d229ac288960c5cb2d9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-08-03 00:06:25 +0900

    Time: 40 ms (20.70%), Space: 16.9 MB (5.18%) - LeetHub

--- 186-reverse-words-in-a-string-ii/186-reverse-words-in-a-string-ii.cpp
+++ 186-reverse-words-in-a-string-ii/186-reverse-words-in-a-string-ii.cpp
@@ -4,11 +4,11 @@ public:
     {
         const size_t SIZE = s.size();
         stack<string> st;
-        vector<char> result;
+        //vector<char> result;
         string temp = "";
         size_t count = 0;
         
-        // 단어별로 만들어서 넣기
+        // 단어별로 만들어서 넣기, 스택에 넣기
         for (const auto& ch : s) {
             count++;
             if (ch != ' ') {
@@ -21,17 +21,19 @@ public:
             
         }
         
+        s.clear();
+        // 스택에서 뽑아서, 다시 만들기
         while (!st.empty()) {
             size_t st_size = st.size();
             string str = st.top(); st.pop();
             for (const auto& ch : str) {
-                result.push_back(ch);
+                s.push_back(ch);
             }
             if (st_size > 1) {
-                result.push_back(' ');
+                s.push_back(' ');
             }
         }
         
-        s = result;
+        //s = result;
     }
 };
