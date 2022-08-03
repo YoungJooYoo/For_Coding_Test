c0d7e207886dd0193f511a7a41547679bd49eb39
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-08-04 00:27:52 +0900

    Time: 1537 ms (5.00%), Space: 599.9 MB (5.03%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,50 @@
+class Solution 
+{
+public:
+    bool is_palindromic (const string& str)
+    {
+        const size_t STR_SIZE = str.size();
+        
+        if (STR_SIZE == 1) {
+            return true;
+        }
+        
+        int i = 0;
+        int j = STR_SIZE - 1;
+        
+        while (i < j) {
+            if (str[i] != str[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    int countSubstrings(string s) 
+    {
+        const size_t STR_SIZE = s.size();
+        vector<string> sub_str;
+        int count = 0;
+        
+        // 서브스트링 전부 구하기
+        for (size_t i = 0; i < STR_SIZE; i++) {
+            string temp;
+            for (size_t j = i; j < STR_SIZE; j++) {
+                temp += s[j];
+                sub_str.push_back(temp);
+            }
+        }
+        
+        // Palindromic 확인하기
+        for (const auto& str : sub_str) {
+            if (is_palindromic(str) == true) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
