bee3a743095d724d5e4167ef0a6204baf8a7379d
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-08-13 16:30:42 +0900

    Time: 6 ms (21.38%), Space: 6.5 MB (60.81%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,25 +1,23 @@
 class Solution 
 {
 public:
-    void recursive(int k, int n, int i)
+    void recursive(int k, int n, int index)
     {
         if (temp.size() == k && n == 0) {
-            
             result.push_back(temp);
-            
             return;
         }
-        for(int j=i;j<10;j++)
-        {
-            temp.push_back(j);
-            recursive(k,n-j,j+1);
+        
+        for (int i = index; i < 10; i++) {
+            temp.push_back(i);
+            recursive(k, n - i, i + 1);
             temp.pop_back();
         }
     }
     
     vector<vector<int>> combinationSum3(int k, int n) 
     {
-        recursive(k,n,1);
+        recursive(k, n, 1); // k, n, 1부터 시작
         return result;
     }
     
