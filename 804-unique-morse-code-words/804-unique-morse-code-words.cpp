ee004920f5c4dc82a861ee9ada82423e4c065c9a
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-08-18 22:51:03 +0900

    Time: 8 ms (34.91%), Space: 8.5 MB (88.09%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -4,8 +4,8 @@ public:
     int uniqueMorseRepresentations(vector<string>& words) 
     {
         for (const auto& word : words) {
-            string curr = "";
-            for (const auto& ch : word) {
+            string curr = ""; // 모스부호 단어 만들 문자열
+            for (const auto& ch : word) { // 문자열의 알파벳을 모스부호화 메칭하기
                 curr += most_codes[ch - 97];
             }
             freq.insert(curr);
