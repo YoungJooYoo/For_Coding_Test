52d9460784f30790bfd20fd7b406a8e9cfbb0913
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-08-19 10:59:06 +0900

    Time: 25 ms (53.28%), Space: 19.7 MB (82.05%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,4 +1,4 @@
-class Solution 
+ class Solution 
 {
 public:
     int trap(vector<int>& height) 
@@ -21,3 +21,16 @@ public:
         return water;
     }
 };
+
+/*
+
+풀이법 :
+
+투포인터로 양 끝에서 시작한다.
+
+양 끝에 height로 부터 최소 값을 취합 후,
+그 최소 높이를 기준으로 높이를 실시간으로 계산한다.
+
+최소 높이도 계속 갱신한다.
+
+*/
