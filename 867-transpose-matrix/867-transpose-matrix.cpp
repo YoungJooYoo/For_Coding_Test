9a3e060a5ee1069a2998490558f22347553cc9c9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-09-01 16:43:38 +0900

    Time: 19 ms (55.77%), Space: 10.6 MB (78.47%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -9,16 +9,9 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                result[i][j] = matrix[j][i];
-                //cout << matrix[j][i] << " ";
             }
         }
         
         return result;
     }
 };
-
-/*
-00 01 02
-10 11 12
-20 21 22
-*/
