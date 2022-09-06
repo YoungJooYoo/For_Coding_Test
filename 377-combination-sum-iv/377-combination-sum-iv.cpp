60753548ea09f999f147a6f1aa6d67ee04e6f883
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-09-07 00:00:23 +0900

    Time: 3 ms (60.08%), Space: 6.8 MB (29.78%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -19,7 +19,7 @@ public:
     
     int combinationSum4(vector<int>& nums, int target) 
     {
-        vector<int> dp(target + 1, -1);
+        vector<int> dp(target + 1, -1); // dp배열의 인덱스가 원소이다.
         dp[0] = 1;
         
         recursive(nums, target, dp);
@@ -27,3 +27,17 @@ public:
         return dp[target];
     }
 };
+
+/*
+풀이법 : 
+dp 배열의 인덱스 즉, 
+해당 인덱스 숫자를 만들기위한 방법의 수를 계산한다.
+
+
+즉, index 0 1 2 3 4
+    count 1 1 2 4 7
+index 4번, 즉 숫자 4를 위해 나올 수 있는 조합의 수는
+1 + 2 + 4 = 7이 된다.
+
+
+*/
