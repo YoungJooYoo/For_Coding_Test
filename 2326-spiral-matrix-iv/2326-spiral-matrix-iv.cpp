5f1f71145cdd2e52f530d7cf2973186ea17a76cc
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-09-14 18:10:12 +0900

    Time: 547 ms (14.38%), Space: 127.5 MB (99.14%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int n, int m, ListNode* head) 
+    {
+        vector<vector<int>> result(n, vector<int>(m, -1));
+        int i = 0;
+        int j = 0;
+
+        while (head != NULL)
+        {
+            if (j < m)
+            {
+                while (head != NULL && j < m && result[i][j] == -1)
+                {
+                    result[i][j] = head->val;
+                    head = head->next;
+                    j++;
+                }
+                if (head == NULL)
+                    break;
+                i++;
+                j--;
+            }
+            if (i < n)
+            {
+                while (head != NULL && i < n && result[i][j] == -1)
+                {
+                    result[i][j] = head->val;
+                    head = head->next;
+                    i++;
+                }
+                i--;
+                j--;
+            }
+            if (j >= 0)
+            {
+                while (head != NULL && j >= 0 && result[i][j] == -1)
+                {
+                    result[i][j] = head->val;
+                    head = head->next;
+                    j--;
+                }
+                j++;
+                i--;
+            }
+            if (i >= 0)
+            {
+                while (head != NULL && i >= 0 && result[i][j] == -1)
+                {
+                    result[i][j] = head->val;
+                    head = head->next;
+                    i--;
+                }
+                i++;
+                j++;
+            }
+            
+            n--;
+            m++;
+        }
+		// Rest values are itself -1.
+        return result;
+    }
+};
