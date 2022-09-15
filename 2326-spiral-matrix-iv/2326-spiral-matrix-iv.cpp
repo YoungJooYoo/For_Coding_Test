9ccdf6a9db6bb8262007cb57209994c17070aa40
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-09-15 13:27:34 +0900

    Time: 287 ms (89.55%), Space: 127.7 MB (92.04%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -10,40 +10,40 @@ public:
         {
             if (j < m)
             {
-                while (head != NULL && j < m && result[i][j] == -1)
+                while (head != NULL && j < m && result[i][j] == -1) // -> 방향으로 진행
                 {
                     result[i][j] = head->val;
                     head = head->next;
                     j++;
                 }
-                i++;
-                j--;
+                i++; // 세로 한칸 내려주기
+                j--; // 세로방향 길이 1 줄이기
             }
             if (i < n)
-            {
-                while (head != NULL && i < n && result[i][j] == -1)
+            { 
+                while (head != NULL && i < n && result[i][j] == -1) // ↓ 방향으로 진행
                 {
                     result[i][j] = head->val;
                     head = head->next;
                     i++;
                 }
-                i--;
-                j--;
+                i--; // 세로로 한칸 위 올려주기
+                j--; // 가로 방향 길이 1 줄이기
             }
             if (j >= 0)
             {
-                while (head != NULL && j >= 0 && result[i][j] == -1)
+                while (head != NULL && j >= 0 && result[i][j] == -1) // <- 방향으로 진행
                 {
                     result[i][j] = head->val;
                     head = head->next;
                     j--;
                 }
-                j++;
-                i--;
+                j++; 
+                i--; // 한칸 업
             }
-            if (i >= 0)
+            if (i >= 0) 
             {
-                while (head != NULL && i >= 0 && result[i][j] == -1)
+                while (head != NULL && i >= 0 && result[i][j] == -1) // 위로 가는 방향으로 진행
                 {
                     result[i][j] = head->val;
                     head = head->next;
@@ -56,7 +56,7 @@ public:
             n--;
             m++;
         }
-		// Rest values are itself -1.
+
         return result;
     }
 };
