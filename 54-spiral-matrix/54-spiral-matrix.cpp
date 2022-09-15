af3058b24b7bacebf87b88680e66029d99e4c44a
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-09-15 13:03:39 +0900

    Time: 3 ms (43.70%), Space: 6.9 MB (72.16%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -5,31 +5,31 @@ public:
     {
         vector<int> result;
         int left = 0;
-        int right = matrix[0].size() - 1;
+        int right =  matrix[0].size() - 1;
         int top = 0;
         int bottom = matrix.size() - 1;
         
         while (left <= right and top <= bottom) {
-            for (int i = left; i <= right; i++) {
+            for (int i = left; i <= right; i++) { // 오른쪽 방향
                 result.push_back(matrix[top][i]);
             }
             top++;
-
-            for (int i = top; i <= bottom; i++) {
+            
+            for (int i = top; i <= bottom; i++) { // 아래방향
                 result.push_back(matrix[i][right]);
             }
             right--;
             
-            if (left > right || top > bottom) {
+            if(left > right || top > bottom) {
                 break;
             }
-
-            for (int i = right; i >= left; i--) {
+            
+            for (int i = right; i >= left; i--) { // 왼쪽방향
                 result.push_back(matrix[bottom][i]);
             }
             bottom--;
-
-            for (int i=bottom; i >= top; i--) {
+            
+            for (int i = bottom; i >= top; i--) { // 위방향
                 result.push_back(matrix[i][left]);
             }
             left++;
