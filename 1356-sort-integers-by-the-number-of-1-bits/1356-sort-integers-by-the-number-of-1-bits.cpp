4bbc726ea0ef036cf5c00e1e787d8f56aab2389b
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-09-27 15:08:05 +0900

    Time: 22 ms (31.50%), Space: 10.9 MB (19.40%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,36 @@
+class Solution 
+{
+public:
+    int countsetbits(int n)
+    {
+        int count = 0;
+        while (n)
+        {
+            if (n % 2 != 0)
+            count++;
+            n /= 2;
+        }
+        
+        return count;
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) 
+    {
+        map<int, vector<int>> mp; // [key 비트 수]-[value 숫자들 ] 
+        vector<int> result;
+        
+        sort(arr.begin(), arr.end());
+        
+        for (int i = 0; i < arr.size(); i++) {
+            mp[countsetbits(arr[i])].push_back(arr[i]); // 1의 비트의 갯수에 따라 분류해서 맵에 저장
+        }
+        
+        for (auto it = mp.begin(); it!=mp.end(); it++) {
+            for (const int& elem : it->second) {
+            result.push_back(elem);
+            }
+        }
+        
+        return result;
+    }
+};
