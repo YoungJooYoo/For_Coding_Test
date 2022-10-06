7dbbb8f210e6e092c411db9a15c10676ff0666f2
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-06 20:35:08 +0900

    Time: 694 ms (33.85%), Space: 130.8 MB (19.24%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -15,14 +15,14 @@ public:
     {
         auto itr = mp[key].upper_bound(timestamp);
         
-        if (itr == mp[key].begin())
+        if (itr == mp[key].begin()) // begin하고 같으면 존재하지 않음
         {
             return "";
         }
         
         itr--;
 
-        int time = itr->first;
+        int time = itr->first; // mp[key] 접근 후 map에 관련 itr이므로 first는 time
         
         return mp[key][time];
     }
