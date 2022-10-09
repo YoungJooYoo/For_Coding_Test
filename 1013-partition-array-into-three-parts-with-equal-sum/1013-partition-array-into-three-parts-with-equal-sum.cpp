f93d1fce6c875397a82f7cd151d2dc99d5ef5fc1
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-09 23:53:13 +0900

    Time: 147 ms (8.77%), Space: 32.6 MB (14.32%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
@@ -3,7 +3,7 @@ public:
     bool canThreePartsEqualSum(vector<int>& arr) 
     {
         int sum = accumulate(arr.begin(), arr.end(), 0); 
-        int part = 0;
+        int part_sum = 0;
         int count = 0;
         
         if (sum % 3 != 0) { // 3분할이니, 3으로 안나누어 떨어지면 안된다.
@@ -11,14 +11,14 @@ public:
         }
         
         for (const int& elem : arr) {
-            part += elem;
+            part_sum += elem;
                 
-            if (part == (sum / 3)) {
+            if (part_sum == (sum / 3)) { // 합이 3으로 나누면 몫이 0이 되는 시점에,
                 count++;
-                part = 0;
+                part_sum = 0;
             }
         }
         
-        return (count >= 3) ? true : false;
+        return (count >= 3) ? true : false; // >= 3 이유는 0 0 0 0과 같이 어떻게 3분할 해도되는 숫자 때문
     }
 };
