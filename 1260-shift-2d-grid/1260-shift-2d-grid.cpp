65c2bf745cae82943ea4a72bb8a1afeec56c7e68
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-13 23:58:00 +0900

    Update 1260-shift-2d-grid.cpp

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -5,7 +5,7 @@ public:
         int n = grid.size();
         int m = grid[0].size();     
         
-        for (int k = 0; k < x; k++){
+        for (int k = 0; k < x; k++) {
             int temp;
             for (int i = n - 1; i >= 0; i--) { // 배열을 뒤에서 부터 읽는다.
                 for (int j = m - 1; j >= 0; j--) {
