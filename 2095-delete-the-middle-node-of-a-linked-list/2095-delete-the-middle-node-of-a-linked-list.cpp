d08baecdeea1dc82d82eafe7c7bd2876cf878956
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-14 13:17:44 +0900

    Time: 2523 ms (5.04%), Space: 294.7 MB (64.88%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -2,6 +2,30 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) 
     {
+        if (head->next == nullptr) {
+            return nullptr;
+        }
+            
+        auto slow = head;
+        auto fast = head->next->next;
+        
+        // slow가 멈추는 위치가 중간위치
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return head;
+    }
+};
+
+/*
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) 
+    {
         ListNode* node = head;
         size_t list_length = 0;
         
@@ -28,3 +52,4 @@ public:
         return head;
     }
 };
+*/
