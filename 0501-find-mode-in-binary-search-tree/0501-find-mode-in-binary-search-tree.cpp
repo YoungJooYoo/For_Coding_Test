2144969ad9349fd3b6ed662561efae211d103844
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-18 16:21:12 +0900

    Time: 51 ms (29.11%), Space: 25.3 MB (59.87%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(TreeNode* root) {
+        //base case
+        if(!root) return;
+        
+        //recursive case
+        map[root->val]++;
+        fre = max(fre, map[root->val]);
+        solve(root->left);
+        solve(root->right);
+        
+        return;
+    }
+    
+    vector<int> findMode(TreeNode* root) {
+        fre = 0;
+        solve(root);
+        vector<int> res;
+        
+        for(auto &x: map){
+            if(x.second == fre) res.push_back(x.first);
+        }
+        
+        return res;
+    }
+private:
+    unordered_map<int, int> map;
+    int fre;
+};
