2ba98be1bcca59eeb9ef64a05318d531d548797e
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-24 23:08:39 +0900

    Time: 156 ms (56.67%), Space: 14.5 MB (71.70%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        vector<int> dp(amount + 1, INT_MAX - 1); // i <= amount 까지므로 배열 길이는 amount + 1
+        dp[0] = 0; 
+        
+        for (size_t i = 1; i <= amount; i++) { 
+            for (const int& coin : coins) {
+                if (coin <= i) {
+                    dp[i] = min(dp[i], dp[i - coin] + ONE_COIN); // dp[i - coin] + 1  == dp[value - coin종류] + 1이유는
+                }
+            }
+        }
+        
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+private:
+    enum { ONE_COIN = 1 };
+};
