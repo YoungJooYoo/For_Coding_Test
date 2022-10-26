60e99fbd829aafa7e2f9b2429fd9203e5a9956d5
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-26 18:15:18 +0900

    Time: 238 ms (39.17%), Space: 101.3 MB (22.09%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -6,27 +6,27 @@ public:
         
         for (int i = 0; i < n; i++) {
             mp[str[i] - 'a']++;
-            if(mp[str[i] - 'a'] > 1) return false; // if we found duplicates return false
+            if (mp[str[i] - 'a'] > 1) return false; // if we found duplicates return false
         }
         
         return true;
     }
     
-    void helper(vector<string>& arr, int i, const size_t ARR_SIZE, string curr)
+    void helper(vector<string>& arr, int i, const size_t ARR_SIZE, string curr_str)
     {
-        if (is_unique(curr) == false) return; // if we have got duplicate int our result then simply return
+        if (is_unique(curr_str) == false) return; // if we have got duplicate int our result then simply return
         
         if (i == ARR_SIZE) { // 재귀 종료조건
-            int curr_size = curr.size();
-            if (curr_size > maxi) {
-                maxi = max(maxi, curr_size); // update maxi
+            int curr_str_size = curr_str.size();
+            if (curr_str_size > maxi) {
+                maxi = max(maxi, curr_str_size); // update maxi
             }
             
             return;
         }
         
-        helper(arr, i + 1, ARR_SIZE, curr + arr[i]); // we have two options either include the curr string or exclude  // include the curr string
-        helper(arr, i + 1, ARR_SIZE, curr); // exclude the curr string
+        helper(arr, i + 1, ARR_SIZE, curr_str + arr[i]); // we have two options either include the curr string or exclude  // include the curr string
+        helper(arr, i + 1, ARR_SIZE, curr_str); // exclude the curr string
     }
     
     int maxLength(vector<string>& arr) 
