750e3be043437335283ba5334e1f5e2eb6c679ef
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-10-26 16:21:10 +0900

    Time: 130 ms (48.88%), Space: 101.3 MB (22.09%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool is_unique(string& str) {
+        int n = str.size();
+        vector<int> mp(26, 0); // mp will store the count of all the characters in string
+        
+        for (int i = 0; i < n; i++) {
+            mp[str[i] - 'a']++;
+            if(mp[str[i] - 'a'] > 1) return false; // if we found duplicates return false
+        }
+        
+        return true;
+    }
+    
+    void helper(vector<string>& arr, int i, const size_t ARR_SIZE, string curr)
+    {
+        if (is_unique(curr) == false) return; // if we have got duplicate int our result then simply return
+        
+        if (i == ARR_SIZE) { // 재귀 종료조건
+            int curr_size = curr.size();
+            if (curr_size > maxi) {
+                maxi = max(maxi, curr_size); // update maxi
+            }
+            
+            return;
+        }
+        
+        helper(arr, i + 1, ARR_SIZE, curr + arr[i]); // we have two options either include the curr string or exclude  // include the curr string
+        helper(arr, i + 1, ARR_SIZE, curr); // exclude the curr string
+    }
+    
+    int maxLength(vector<string>& arr) 
+    {
+        helper(arr, 0, arr.size(), "");
+        
+        return maxi;
+    }
+    
+private:
+    int maxi = 0; // function for checking uniqueness of the string
+};
