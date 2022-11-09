47459a0d5d5e00b6259f1051646c971efdc416a9
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-11-10 01:01:39 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (27.48%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    bool hasAlternatingBits(int num) {
-        int flag=num%2;
-        num=num/2;
-        while(num)
-        {
-            if(flag==num%2)
-                return false;
-            else
-                flag=num%2;
+    bool hasAlternatingBits(int num) 
+    {
+        int flag = num % 2;
+        num = num / 2;
+        
+        while(num) {
+            if (flag == num % 2) return false;
+            else flag = num % 2;
             num/=2;
-            
         }
+        
         return true;
     }
 };
