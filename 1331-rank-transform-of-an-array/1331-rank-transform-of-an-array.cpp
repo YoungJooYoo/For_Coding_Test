211c7f621c2bcd3e4541956e7d726672798da412
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-11-12 23:49:58 +0900

    Time: 234 ms (44.41%), Space: 46.2 MB (21.14%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-vector<int> arrayRankTransform(vector<int>& arr) {
-	set<int> st(arr.begin(),arr.end());
-	unordered_map<int,int> m;
-	int rank=1;
-	for(auto &i: st)  m[i]=rank++;
-	for(auto &i: arr) i = m[i];
-	return arr;
-}
+    vector<int> arrayRankTransform(vector<int>& arr) 
+    {
+        set<int> set(arr.begin(), arr.end());
+        unordered_map<int, int> mp; // index, value;
+        int rank = 1;
+        
+        for (const auto& num : set) {
+            mp[num] = rank++;
+        }
+        
+        for (size_t i = 0; i < arr.size(); i++) {
+            arr[i] = mp[arr[i]];
+        }
+        
+        return arr;
+    }
 };
