8624dfb0aee102ca2525f1e7d00b313eaf63a56d
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-11-16 23:22:33 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (37.93%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -5,8 +5,6 @@ public:
         int count = 0;
         size_t count_r = 0;
         size_t count_l = 0;
-        // RL RRRRLLLL
-        // rl 을 1:1 대응으로 분리 시치는 것
         
         for (const auto& ch : s) {
             if (ch == 'R') {
