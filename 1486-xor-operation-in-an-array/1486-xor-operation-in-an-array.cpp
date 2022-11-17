ad38be6e65c85a917f09614f196e8103941289fd
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-11-17 10:39:42 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (41.45%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -7,7 +7,7 @@ public:
         
         for (int i = 0; i < n; i++) {
             num = start + (2 * i);
-            result = result ^ num;
+            result ^= num;
         }
         
         return result;
