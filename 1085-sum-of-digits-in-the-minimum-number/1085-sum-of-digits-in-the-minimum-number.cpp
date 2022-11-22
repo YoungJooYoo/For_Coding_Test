3ef8e80cdd226176be693eaabdc57db04f893277
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2022-11-22 11:23:04 +0900

    Time: 3 ms (75.44%), Space: 8 MB (87.72%) - LeetHub

---
+++ 1085-sum-of-digits-in-the-minimum-number/1085-sum-of-digits-in-the-minimum-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfDigits(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        
+        int temp = nums[0];
+        int sum = 0;
+        
+        while(temp != 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        return sum % 2 != 0 ? 0 : 1;
+    }
+};
