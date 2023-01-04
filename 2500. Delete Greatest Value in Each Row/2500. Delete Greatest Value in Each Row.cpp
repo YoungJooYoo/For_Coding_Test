6c72df4d94c75eea05113ac1d79b79a1f2bf4683
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2023-01-05 08:07:21 +0900

    Update 2500. Delete Greatest Value in Each Row.cpp

--- 2500. Delete Greatest Value in Each Row/2500. Delete Greatest Value in Each Row.cpp
+++ 2500. Delete Greatest Value in Each Row/2500. Delete Greatest Value in Each Row.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) 
     {
-        set<int> nums;
         int ans = 0;
 
         // 행마다 정렬 후, 열중의 최대 값만 모아서 계산하기
@@ -11,11 +10,11 @@ public:
         }    
 
         for (size_t j = 0; j < grid[0].size(); ++j) {
-            int max = -1;
+            int max_num = -1;
             for (size_t i = 0; i < grid.size(); ++i) {
-                if (max < grid[i][j]) max = grid[i][j];
+               max_num = max(max_num, grid[i][j]);
             }
-            ans += max;
+            ans += max_num;
         }
 
         return ans;
