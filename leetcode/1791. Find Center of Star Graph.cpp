a018f98d500189d5a65299bf87cd58cf7b1abc9a
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2023-02-10 10:08:14 +0900

    Create 1791. Find Center of Star Graph.cpp
    // https://leetcode.com/problems/find-center-of-star-graph/description/?orderBy=most_votes&languageTags=cpp

---
+++ leetcode/1791. Find Center of Star Graph.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) 
+    {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        }
+        else {
+            return edges[0][1];
+        }
+
+        return 0;
+    }
+};
+
+// https://leetcode.com/problems/find-center-of-star-graph/description/?orderBy=most_votes&languageTags=cpp
