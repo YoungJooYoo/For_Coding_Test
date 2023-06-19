605943f1a307f5be74179d699d95338495a6df76
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2023-06-20 08:55:22 +0900

    Create 2733. Neither Minimum nor Maximum.cpp
    https://leetcode.com/problems/neither-minimum-nor-maximum/description/

---
+++ leetcode/2733. Neither Minimum nor Maximum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) 
+    {
+        if (nums.size() < 3) return -1;
+
+        sort(nums.begin(), nums.end());
+
+        return nums[1];
+    }
+};
+
+// https://leetcode.com/problems/neither-minimum-nor-maximum/description/
