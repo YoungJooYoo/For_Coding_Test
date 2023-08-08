f961cf26acff0032d9483fe26a1b52a431758d29
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2023-08-08 11:54:57 +0900

    Create 2769. Find the Maximum Achievable Number.cpp
    https://leetcode.com/problems/find-the-maximum-achievable-number/

---
+++ Leet Code/2769. Find the Maximum Achievable Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) 
+    {
+        return (num + 2 * t);
+    }
+};
+
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
