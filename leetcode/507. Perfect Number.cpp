14729c1d80ca9e5c927e1140754014fe0440550c
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2024-06-05 09:52:16 +0900

    Create 507. Perfect Number.cpp
    https://leetcode.com/problems/perfect-number/description/

---
+++ leetcode/507. Perfect Number.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    bool checkPerfectNumber(const int num) 
+    {
+        int sum = 0;
+
+        for (int i = 1; i < num; ++i)
+        {
+            if (num % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum == num;    
+    }
+};
+
+// https://leetcode.com/problems/perfect-number/description/
