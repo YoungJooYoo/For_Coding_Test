ab0ffb36be8ba2bb1e3bd18ce2104ba0717ac3cf
Gummy <31311232+YoungJooYoo@users.noreply.github.com>
2024-12-25 13:45:25 +0900

    Update 515. Find Largest Value in Each Tree Row.cpp

--- leetcode/515. Find Largest Value in Each Tree Row.cpp
+++ leetcode/515. Find Largest Value in Each Tree Row.cpp
@@ -56,4 +56,50 @@ private:
     vector<int> mResult;
 };
 
+// solution 2
+
+class Solution 
+{
+public:
+    vector<int> largestValues(const TreeNode* const root)
+    {
+        if (root == nullptr) 
+        {
+            return mResult;
+        }
+
+        traverseTree(root, 0);
+        extractMaxValuesPerDepth();
+
+        return mResult;
+    }
+
+private:
+    void traverseTree(const TreeNode* const node, const int depth)
+    {
+        if (node == nullptr) 
+        {
+            return;
+        }
+
+        mDepthToMaxHeap[depth].push(node->val);
+        traverseTree(node->left, depth + 1);
+        traverseTree(node->right, depth + 1);
+    }
+
+    void extractMaxValuesPerDepth()
+    {
+        int depth = 0;
+        while (mDepthToMaxHeap.count(depth))
+        {
+            mResult.push_back(mDepthToMaxHeap[depth].top());
+            ++depth;
+        }
+    }
+
+    unordered_map<int, priority_queue<int>> mDepthToMaxHeap; // 각 깊이에 대한 최대 힙
+    vector<int> mResult;
+};
+
+
 // https://leetcode.com/problems/find-largest-value-in-each-tree-row/description/?envType=daily-question&envId=2024-12-25
